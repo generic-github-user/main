4597d9e83b2459e74f9ad866172bacdbc1e5140d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-08-14 11:53:50 -0400

    Added "Network" class and constructor

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ class Node {
             this.output;
       }
 }
+
+class Network {
+      constructor(inputs, outputs) {
+            var nodes = [];
+            for (var i = 0; i < inputs; i ++) {
+                  nodes.push(new Node("Data/Input"));
+            }
+            for (var i = 0; i < outputs; i ++) {
+                  nodes.push(new Node("Data/Output"));
+            }
+            this.nodes = nodes;
+      }
+}
