e176b895ec5528938e91990c59129efcb0995a62
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-08-14 13:03:27 -0400

    Added unique node UUIDs

--- script.js
+++ script.js
@@ -4,12 +4,36 @@ const random = function (minimum, maximum) {
 const random_item = function (array) {
       return array[Math.floor(Math.random() * array.length)];
 }
+const UUID = function () {
+      function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+const get_node = function (id) {
+      return nodes.find(x => x.id == id);
+}
 
+var nodes = [];
 class Node {
       constructor(type) {
             this.type = type;
             this.inputs = [];
-            this.output;
+            this.output = undefined;
+
+            var id = UUID();
+
+            do {
+                  id = UUID();
+            }
+            while (nodes.find(x => x.id == id) !== undefined)
+
+            this.id = id;
+            nodes.push(this);
+      }
+}
       }
 }
 
