93282838d91b502a78b65ebf3243d3b98f70e44c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-08-17 22:03:51 -0400

    Added brightness to nodes - #43

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -76,6 +76,11 @@ const get_node = function (id) {
       return nodes.find(x => x.id == id);
 }
 
+const shade_color = function (color, percent) {
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
 var nodes = [];
 var node_inputs = [];
 var node_outputs = [];
@@ -301,7 +306,10 @@ class network {
                               settings.node_types.forEach(
                                     (node_type) => {
                                           if (node.type == node_type.name) {
-                                                circle.style.fill = node_type.color;
+                                                circle.style.fill = shade_color(
+                                                      node_type.color,
+                                                      map(node.value, min, max, -1, 1)
+                                                );
                                           }
                                     }
                               );
