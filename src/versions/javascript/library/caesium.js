f3a45c9b3b27544c32c5bae7980bcf5d35e00b43
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-08-20 08:28:27 -0400

    Separated Caesium project into main library and visualization app
    #5

---
+++ src/versions/javascript/library/caesium.js
@@ -0,0 +1,293 @@
+// ASCII logo art displayed in console
+// http://www.network-science.de/ascii/
+console.log("%c\
+    _____            ______   _____  _____  _    _  __  __   \n\
+   / ____|    /\\    |  ____| / ____||_   _|| |  | ||  \\/  |  \n\
+  | |        /  \\   | |__   | (___    | |  | |  | || \\  / |  \n\
+  | |       / /\\ \\  |  __|   \\___ \\   | |  | |  | || |\\/| |  \n\
+  | |____  / ____ \\ | |____  ____) | _| |_ | |__| || |  | |  \n\
+   \\_____|/_/    \\_\\|______||_____/ |_____| \\____/ |_|  |_|  \n\
+                                                             \
+", "background: #e8efff; color: #4319ff; font-weight: 1000;");
+
+
+
+// Generate a random number in between a minimum value and a maximum value
+const random = function (minimum, maximum) {
+      return minimum + (Math.random() * (maximum - minimum));
+}
+// Select a random element from a given array
+const random_item = function (array) {
+      return array[Math.floor(Math.random() * array.length)];
+}
+// Clone an object so that the new object does not contain a reference to the original object; either object may be altered without affecting the other
+const clone = function (object) {
+      // Convert JSON object to a string, then parse it back into a new object and return the object
+      return JSON.parse(JSON.stringify(object));
+}
+// Find a node globally given its ID
+const get_node = function (id) {
+      return nodes.find(x => x.id == id);
+}
+
+// Settings for networks
+var settings = {
+      "node_types": [
+            {
+                  "name": "Data/Input",
+                  // Green
+                  "color": "#3f9b41"
+            },
+            {
+                  "name": "Data/Output",
+                  // Light blue
+                  "color": "#afcfff"
+            },
+            {
+                  "name": "Data/Value",
+                  // Orange
+                  "color": "#ff8300"
+            },
+            {
+                  "name": "Operation/Addition",
+                  // Purple
+                  "color": "#c854ff"
+            },
+            {
+                  "name": "Operation/Multiplication",
+                  // Red
+                  "color": "#f92c2c"
+            }
+      ],
+      "population_size": 100,
+      "visualization": {
+            "label": undefined,
+            "label": undefined,
+            "size": undefined,
+            "brightness": undefined
+      }
+};
+
+const cs = {
+      // List of all nodes
+      "nodes": [],
+      // List of nodes with inputs
+      "node_inputs": [],
+      // List of nodes with outputs
+      "node_outputs": [],
+      // List of output nodes
+      "input_nodes":[],
+      // List of output nodes
+      "output_nodes": [],
+}
+
+// Generate a random UUID
+// Based on https://stackoverflow.com/a/105074
+var uuids = [];
+cs.UUID = function () {
+      // Generate a random string of four hexadecimal digits
+      function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+      }
+      // Generate UUID from substrings
+      var id;
+      do {
+            id = s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4();
+      }
+      while (uuids.indexOf(id) !== -1)
+
+      uuids.push(id);
+      return id;
+}
+
+cs.node = class {
+      constructor(information) {
+            this.type = information.type;
+            if (information.type == "Data/Input") {
+                  cs.input_nodes.push(this);
+
+                  // Cannot be undefined
+                  this.value = 0;
+                  cs.node_outputs.push(this);
+            }
+            else if (information.type == "Data/Output") {
+                  cs.output_nodes.push(this);
+
+                  cs.node_inputs.push(this);
+                  this.value = 0;
+                  cs.node_outputs.push(this);
+            }
+            else if (information.type == "Data/Value") {
+                  this.value = information.value;
+                  cs.node_outputs.push(this);
+            }
+            else if (information.type == "Operation/Addition") {
+                  cs.node_inputs.push(this);
+                  this.value = 0;
+                  cs.node_outputs.push(this);
+            }
+            else if (information.type == "Operation/Multiplication") {
+                  cs.node_inputs.push(this);
+                  this.value = 0;
+                  cs.node_outputs.push(this);
+            }
+            else {
+                  console.error("'" + information.type + "'" + "is not a valid node type. Please use 'Data/Input', 'Data/Output', 'Data/Value', 'Operation/Addition', or 'Operation/Multiplication'.");
+            }
+
+            var id = cs.UUID();
+            this.id = id;
+            cs.nodes.push(this);
+      }
+}
+
+cs.connection = class {
+      // Constructor for creating a connection between two nodes within a network
+      constructor(source, destination) {
+            this.source = source;
+            this.destination = destination;
+      }
+}
+
+cs.network = class {
+      // Constructor for creating a network or computation graph
+      constructor(inputs, outputs) {
+            this.inputs = inputs;
+            this.outputs = outputs;
+
+            var nodes = [];
+            cs.node_inputs = [];
+            cs.node_outputs = [];
+            cs.input_nodes = [];
+            cs.output_nodes = [];
+            for (var i = 0; i < inputs; i ++) {
+                  nodes.push(
+                        new cs.node({
+                              "type": "Data/Input"
+                        })
+                  );
+            }
+            for (var i = 0; i < outputs; i ++) {
+                  nodes.push(
+                        new cs.node({
+                              "type": "Data/Output"
+                        })
+                  );
+            }
+            for (var i = 0; i < Math.round(random(10, 20)); i ++) {
+                  nodes.push(
+                        new cs.node({
+                              "type": "Data/Value",
+                              "value": random(-1, 1)
+                        })
+                  );
+            }
+            for (var i = 0; i < Math.round(random(10, 20)); i ++) {
+                  nodes.push(
+                        new cs.node({
+                              "type": "Operation/Addition"
+                        })
+                  );
+            }
+            for (var i = 0; i < Math.round(random(10, 20)); i ++) {
+                  nodes.push(
+                        new cs.node({
+                              "type": "Operation/Multiplication"
+                        })
+                  );
+            }
+            this.nodes = cs.nodes;
+            this.node_inputs = cs.node_inputs;
+            this.node_outputs = cs.node_outputs;
+            this.input_nodes = cs.input_nodes;
+            this.output_nodes = cs.output_nodes;
+
+            var connections = [];
+            for (var i = 0; i < Math.round(random(50, 100)); i ++) {
+                  connections.push(
+                        // Create a new connection with the constructor function
+                        new cs.connection(
+                              // Connection sources must be nodes with outputs
+                              random_item(this.node_outputs),
+                              // Connection destinations must be nodes with inputs
+                              random_item(this.node_inputs)
+                        )
+                  );
+            }
+            this.connections = connections;
+
+            this.inputs = function (inputs) {
+                  var num_inputs = this.input_nodes.length;
+                  if (inputs.length < num_inputs) {
+                        console.error("The number of inputs you have provided (" + num_inputs + ") is fewer than the number of input nodes in the network (" + num_inputs + "). Please provide " + num_inputs + " inputs.");
+                        return false;
+                  }
+                  else if (inputs.length > num_inputs) {
+                        console.error("The number of inputs you have provided (" + num_inputs + ") is fewer than the number of input nodes in the network (" + num_inputs + "). Please provide " + num_inputs + " inputs.");
+                        return false;
+                  }
+                  else if (inputs.length == num_inputs) {
+                        for (var i = 0; i < inputs.length; i ++) {
+                              this.input_nodes[i].value = inputs[i];
+                        }
+                        return inputs;
+                  }
+            }
+
+            this.outputs = function () {
+                  var outputs = [];
+                  this.output_nodes.forEach(
+                        (node) => {
+                              outputs.push(node.value);
+                        }
+                  );
+                  return outputs;
+            }
+
+            // Run one iteration of calculations for node values in network
+            this.update = function () {
+                  // Create a clone of the network so that all nodes can be updated simultaneously, without affecting other values
+                  var network_buffer = clone(this);
+                  this.nodes.forEach(
+                        (node) => {
+                              var type = node.type;
+                              if (type == "Data/Output" || type == "Operation/Addition") {
+                                    node.value = 0;
+                              }
+                              else if (type == "Operation/Multiplication") {
+                                    node.value = 1;
+                              }
+                        }
+                  );
+                  for (var i = 0; i < network_buffer.connections.length; i ++) {
+                        var type = this.connections[i].destination.type;
+                        if (type == "Data/Output" || type == "Operation/Addition") {
+                              this.connections[i].destination.value +=
+                              network_buffer.connections[i].source.value;
+                        }
+                        else if (type == "Operation/Multiplication") {
+                              this.connections[i].destination.value *=
+                              network_buffer.connections[i].source.value;
+                        }
+                  }
+                  this.nodes.forEach(
+                        (node) => {
+                              if (node.type == "Operation/Multiplication" && node.value == 1) {
+                                    node.value = 0;
+                              }
+                        }
+                  );
+
+                  // Return updated network object
+                  return this;
+            }
+      }
+}
+
+// var population = [];
+//
+// for (var i = 0; i < settings.population_size; i ++) {
+//       population.push(new cs.network(5, 5));
+// }
---
+++ src/versions/javascript/projects/network-visualization/javascript/visualization.js
@@ -0,0 +1,232 @@
+// Map one range of numbers to another, given an input value and the two ranges
+// https://stackoverflow.com/a/23202637
+const map = function (num, in_min, in_max, out_min, out_max) {
+      return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+// Change the brightness of a hexadecimal color value
+// https://stackoverflow.com/a/13542669
+const shade_color = function (color, percent) {
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
+const to_percent = {
+      "w": function(number, property) {
+            return (number / svg.getBoundingClientRect().width) * 100;
+      },
+      "h": function(number, property) {
+            return (number / svg.getBoundingClientRect().height) * 100;
+      }
+}
+
+const to_pixels = {
+      "w": function(number, property) {
+            return (number / 100) * svg.getBoundingClientRect().width;
+      },
+      "h": function(number, property) {
+            return (number / 100) * svg.getBoundingClientRect().height;
+      }
+}
+
+const round = function (number, decimals) {
+      var factor = 10 ** decimals
+      return Math.round(number * factor) / factor;
+}
+
+var network = new cs.network(5, 5);
+
+// Update program settings from user inputs
+const update_settings = function () {
+      settings.visualization.label = document.querySelector("#controls-visualization-label").checked;
+      settings.visualization.size = document.querySelector("#controls-visualization-size").checked;
+      settings.visualization.brightness = document.querySelector("#controls-visualization-brightness").checked;
+}
+update_settings();
+
+var svg = document.querySelector("#network-visualization");
+
+var sidebar = document.querySelector("#control-panel");
+var sidebar_hover = false;
+var sidebar_x = 0;
+var sidebar_y = 0;
+sidebar.addEventListener("mouseover", () => {sidebar_hover = true});
+sidebar.addEventListener("mouseout", () => {sidebar_hover = false});
+
+var body_hover = false;
+var body_x = 0;
+var body_y = 0;
+document.body.addEventListener("mouseover", () => {body_hover = true});
+document.body.addEventListener("mouseout", () => {body_hover = false});
+
+var speed = 0.025;
+const update_backgrounds = function () {
+      if (sidebar_hover) {
+            sidebar_x += speed;
+            sidebar_y += speed;
+      }
+      if (body_hover && !sidebar_hover) {
+            body_x -= speed;
+            body_y -= speed;
+      }
+
+      sidebar.style.backgroundPositionX = sidebar_x + "%";
+      sidebar.style.backgroundPositionY = sidebar_y + "%";
+
+      document.body.style.backgroundPositionX = body_x + "%";
+      document.body.style.backgroundPositionY = body_y + "%";
+}
+setInterval(update_backgrounds, 10);
+
+// Display visualization of network
+// This function is only used the first time the network is displayed, as it generates the SVG elements used in the visualization
+network.display = function () {
+      svg.innerHTML = "";
+
+      // Loop through each connection in network
+      this.nodes.forEach(
+            (node) => {
+                  node.display = {};
+
+
+                  var circle = document.createElement("circle");
+                  circle.className = "node";
+
+                  // Randomly select an x coordinate
+                  var x = random(
+                        // Minimum x value - the left side of the screen (with the circle's radius and margin added)
+                        (5 + 3),
+                        // Maximum x value - the right side of the screen (minus the circle's radius and margin)
+                        (100 - 5 - 3)
+
+                  );
+
+                  // Randomly select a y coordinate
+                  var y = random(
+                        // Minimum y value - the top of the screen
+                        (5 + 3),
+                        // Maximum y value - the bottom side of the screen
+                        (100 - 5 - 3)
+                  );
+
+                  // Set x position of circle element within SVG element
+                  circle.setAttribute("cx", (x + "%"));
+                  // Set y position of circle
+                  circle.setAttribute("cy", (y + "%"));
+
+                  var id = cs.UUID();
+                  circle.setAttribute("id", id);
+                  // Store a pointer to the circle element in corresponding connection object
+                  // This must be done by retrieving the id of the element to create a link between the variable and the element
+                  node.display.circle = id;
+
+                  // Add circle element to SVG element's HTML; this must be done with innerHTML or the SVG element will not be updated
+                  svg.innerHTML += circle.outerHTML;
+
+
+                  if (settings.visualization.label) {
+                        var text = document.createElement("text");
+                        text.className = "value-label";
+
+                        var id = cs.UUID();
+                        text.setAttribute("id", id);
+                        node.display.text = id;
+
+                        svg.innerHTML += text.outerHTML;
+                  }
+            }
+      );
+
+      // Render connections
+      // Loop through each connection in network
+      this.connections.forEach(
+            (connection) => {
+                  connection.display = {};
+
+
+                  // Create a new SVG line element
+                  var line = document.createElement("line");
+                  // Apply "connection" CSS class to line element
+                  line.className = "connection";
+
+                  // Origin of line (first node)
+                  line.setAttribute("x1", document.getElementById(connection.source.display.circle).getAttribute("cx"));
+                  line.setAttribute("y1", document.getElementById(connection.source.display.circle).getAttribute("cy"));
+                  // Destination of line (second node)
+                  line.setAttribute("x2", document.getElementById(connection.destination.display.circle).getAttribute("cx"));
+                  line.setAttribute("y2", document.getElementById(connection.destination.display.circle).getAttribute("cy"));
+
+                  // Store reference to line element in corresponding connection object
+                  var id = cs.UUID();
+                  line.setAttribute("id", id);
+                  connection.display.line = id;
+
+                  // Add line HTML to SVG element before circle elements
+                  svg.innerHTML = line.outerHTML + svg.innerHTML;
+            }
+      );
+}
+
+network.update_display = function () {
+      // Render nodes
+      // https://stackoverflow.com/a/4020842
+      var min = Math.min.apply(Math, this.nodes.map(function(x) { return x.value; }));
+      var max = Math.max.apply(Math, this.nodes.map(function(x) { return x.value; }));
+
+      this.nodes.forEach(
+            (node) => {
+                  var circle = document.getElementById(node.display.circle);
+
+                  // Default radius is 2%
+                  var radius = 2;
+                  // Check if node size is enabled in visualization settings
+                  if (settings.visualization.size) {
+                        // Map node values to a 1 to 5 percent range
+                        radius = map(node.value, min, max, 1, 5);
+                  }
+                  // Set radius of circle as percentage
+                  circle.setAttribute("r", radius + "%");
+
+                  settings.node_types.forEach(
+                        (node_type) => {
+                              if (node.type == node_type.name) {
+                                    var color = node_type.color;
+                                    if (settings.visualization.brightness) {
+                                          color = shade_color(
+                                                color,
+                                                map(node.value, min, max, -0.75, 0.75)
+                                          );
+                                    }
+                                    circle.style.fill = color;
+                              }
+                        }
+                  );
+
+
+                  var text = document.getElementById(node.display.text);
+                  if (settings.visualization.label) {
+                        text.style.display = "";
+                        text.innerHTML = round(node.value, 3);
+
+                        // To pixels
+                        radius = to_pixels.w(radius);
+                        text.style.fontSize = radius / 2;
+
+                        // Don't use parseInt
+                        var x = parseFloat(circle.getAttribute("cx"));
+                        var y = parseFloat(circle.getAttribute("cy"));
+                        x -= to_percent.w(text.getBBox().width / 2);
+                        y += to_percent.h(text.getBBox().height / 3);
+                        // Set x position of text element within SVG element
+                        text.setAttribute("x", (x + "%"));
+                        // Set y position of text
+                        text.setAttribute("y", (y + "%"));
+                  }
+                  else {
+                        text.style.display = "none";
+                  }
+            }
+      );
+}
+
+network.display();
+network.update_display();
