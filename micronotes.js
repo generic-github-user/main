e4a2325b0813f32e0d7c9fe6c93318ebaca56333
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-08-26 10:11:34 -0400

    Added stylesheet information to head

--- micronotes.js
+++ micronotes.js
@@ -7,3 +7,24 @@ const replace = function (string, substring_f, substring_r) {
 	} while (substringIndex !== -1)
 	return output;
 }
+const styles = "\n\
+.note {\n\
+      background-color: #FFF;\n\
+      \n\
+      opacity: 0;\n\
+      transition: opacity 1s ease;\n\
+      \n\
+      max-width: 25vw;\n\
+      max-height: 25vw;\n\
+      position: absolute;\n\
+}\n\
+\n";
+
+if (document.querySelector("style")) {
+      document.head.querySelector("style").innerHTML += styles;
+}
+else {
+      var style = document.createElement("style");
+      style.innerHTML += styles;
+      document.head.appendChild(style);
+}
