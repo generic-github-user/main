d7ec76e23937b440e1ba1535ddc85c0762e550d5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-11-10 10:46:11 -0500

    Added training data

--- src/versions/javascript/projects/neuroevolution/script.js
+++ src/versions/javascript/projects/neuroevolution/script.js
@@ -1,3 +1,19 @@
+// var x = [1, 2, 3, 4, 5];
+var x = [
+      [1],
+      [2],
+      [3],
+      [4],
+      [5]
+];
+var y = [
+      [1],
+      [4],
+      [9],
+      [16],
+      [25]
+];
+
 var network = new cs.network({
       "inputs": 1,
       "outputs": 1,
