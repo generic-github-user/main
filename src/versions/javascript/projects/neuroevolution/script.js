f46cd50608cc1474cd9d15392da934d2fc622ac6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-11-10 10:49:39 -0500

    Added evolution loop

--- src/versions/javascript/projects/neuroevolution/script.js
+++ src/versions/javascript/projects/neuroevolution/script.js
@@ -31,3 +31,53 @@ var network = new cs.network({
       },
       "connections": 0
 });
+
+var update_settings = {
+      "iterations": 4,
+      "limit": {
+            "min": -10e3,
+            "max": 10e3
+      }
+};
+
+function update() {
+      var generation = network.evolve({
+            "iterations": 1,
+            "population": 10,
+            "inputs": input_data,
+            "outputs": output_data,
+            "mutate": {
+                  "iterations": 1,
+                  "mutation_rate": 0.5,
+                  "mutation_size": 1,
+                  "nodes": {
+                        "Data/Value": {
+                              "add": [0, 0],
+                              "remove": [0, 0],
+                              "limit": 10,
+                              "init": [-1, 1]
+                        },
+                        "Operation/Addition": {
+                              "add": [0, 0],
+                              "remove": [0, 0],
+                              "limit": 10
+                        },
+                        "Operation/Multiplication": {
+                              "add": [0, 0],
+                              "remove": [0, 0],
+                              "limit": 10
+                        }
+                  },
+                  "connections": {
+                        "add": [0, 10],
+                        "remove": [0, 10],
+                        "limit": 25
+                  }
+            },
+            "update": update_settings,
+            "log": false,
+            "return": "all"
+      });
+}
+
+setInterval(update, 1000);
