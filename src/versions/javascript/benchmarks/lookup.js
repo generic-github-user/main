727eb2eac586ad9f4ca94f8a2ca2430208ba3f4c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-11-13 13:56:28 -0500

    Added node lookup benchmark

---
+++ src/versions/javascript/benchmarks/lookup.js
@@ -0,0 +1,23 @@
+var nodes = new Array(100).fill({
+      "id": cs.UUID(),
+      "value": cs.random()
+});
+var node;
+
+console.time("Test 1");
+node = nodes.find(x => x.id == nodes[49].id);
+console.timeEnd("Test 1");
+
+nodes = {};
+var id;
+for (var i = 0; i < 100; i++) {
+      id = cs.UUID();
+      nodes[id] = {
+            "id": id,
+            "value": cs.random()
+      };
+}
+
+console.time("Test 2");
+node = nodes[nodes[49].id];
+console.timeEnd("Test 2");
