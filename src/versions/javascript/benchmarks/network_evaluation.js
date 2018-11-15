3008f853b50b11062855f84eae9f31bb2525dcab
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-11-14 21:32:25 -0500

    Added comments to network evaluation benchmark script
    #3

--- src/versions/javascript/benchmarks/network_evaluation.js
+++ src/versions/javascript/benchmarks/network_evaluation.js
@@ -1,7 +1,10 @@
 // network_evaluation.js
 
+// Initialize variables
 var start, end, average, inputs, outputs;
+// List of time each test took
 var times = [];
+// Create network to use for testing
 var network = new cs.network({
       "inputs": 20,
       "outputs": 20,
@@ -22,29 +25,38 @@ var network = new cs.network({
       },
       "connections": 500
 });
+// Update settings for network evaluation
 var update_settings = {
-      "iterations": 10,
+      "iterations": 5,
       "limit": {
             "min": -10e3,
             "max": 10e3
       }
 };
 
+// Run 100 speed tests
 for (var i = 0; i < 100; i++) {
       inputs = [];
       for (var j = 0; j < 20; j++) {
             inputs.push(cs.random());
       }
 
+      // Reset values of nodes in network
       network.reset();
+      // Start timer
       start = performance.now();
+      // Evaluate network to find outputs given input data
       outputs = network.evaluate({
             "input": inputs,
             "update": update_settings
-      })
+      });
+      // Stop timer
       end = performance.now();
+      // Calculate elapsed time during test and add to list of times
       times.push(end - start);
 }
+// Find average of test times
 average = cs.average(times);
 
+// Log information about benchmark tests
 console.log("Tests took average of " + average + " milliseconds each.", times);
