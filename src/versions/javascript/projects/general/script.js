82fa6beadcff1d146f044dae0f25d815b2fc94ba
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-11-18 14:06:28 -0500

    Added update loop

--- src/versions/javascript/projects/general/script.js
+++ src/versions/javascript/projects/general/script.js
@@ -1,6 +1,6 @@
 var charset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", " "];
 
-var chars = 5;
+var chars = 10;
 
 var network = new cs.network({
       "nodes": {
@@ -40,7 +40,7 @@ var network = new cs.network({
 });
 
 var update_settings = {
-      "iterations": 1,
+      "iterations": 2,
       "limit": {
             "min": -10e3,
             "max": 10e3
@@ -70,78 +70,12 @@ const decode = function(input, charset) {
 var inputs = [];
 var outputs = [];
 for (var i = 0; i < data.length; i++) {
-      inputs.push(encodeString(data[i].input, charset, chars));
-      outputs.push(encodeString(data[i].output, charset, chars));
+      inputs.push(encode(data[i].input, charset, chars));
+      outputs.push(encode(data[i].output, charset, chars));
 }
 
-a = 0;
-
-network = network.evolve({
-      "iterations": 10,
-      "population": 50,
-      "inputs": inputs,
-      "outputs": outputs,
-      "mutate": {
-            "iterations": 1,
-            "nodes": {
-                  "Value": {
-                        "add": [0, a],
-                        "remove": [0, a],
-                        "limit": 10,
-                        "init": [-1, 1],
-                        "value": {
-                              "mutation_rate": 0.5,
-                              "mutation_size": 1,
-                        }
-                  },
-                  "Addition": {
-                        "add": [0, a],
-                        "remove": [0, a],
-                        "limit": 10
-                  },
-                  "Multiplication": {
-                        "add": [0, a],
-                        "remove": [0, a],
-                        "limit": 10
-                  },
-                  // "Tanh": {
-                  //       "add": [0, a],
-                  //       "remove": [0, a],
-                  //       "limit": 10
-                  // },
-                  // "Sine": {
-                  //       "add": [0, a],
-                  //       "remove": [0, a],
-                  //       "limit": 10
-                  // },
-                  // "Cosine": {
-                  //       "add": [0, a],
-                  //       "remove": [0, a],
-                  //       "limit": 10
-                  // },
-                  // "Abs": {
-                  //       "add": [0, a],
-                  //       "remove": [0, a],
-                  //       "limit": 10
-                  // }
-            },
-            "connections": {
-                  "add": [0, 0],
-                  "remove": [0, 0],
-                  "limit": 50,
-                  "init": [-1, 1],
-                  "value": {
-                        "mutation_rate": 0.5,
-                        "mutation_size": 0.1,
-                  }
-            }
-      },
-      "update": update_settings,
-      "log": true,
-      "return": "network"
-});
-
-// don't use evaluate
+// Maximum of output values
+// don't use evaluate - reset
 const predict = function(input) {
       return decode(
             cs.apply(
@@ -154,3 +88,76 @@ const predict = function(input) {
             charset
       );
 }
+
+a = 1;
+
+const update = function() {
+      network = network.evolve({
+            "iterations": 1,
+            "population": 100,
+            "inputs": inputs,
+            "outputs": outputs,
+            "mutate": {
+                  "iterations": 1,
+                  "nodes": {
+                        "Value": {
+                              "add": [0, a],
+                              "remove": [0, a],
+                              "limit": 10,
+                              "init": [-1, 1],
+                              "value": {
+                                    "mutation_rate": 1,
+                                    "mutation_size": 0,
+                              }
+                        },
+                        "Addition": {
+                              "add": [0, a],
+                              "remove": [0, a],
+                              "limit": 10
+                        },
+                        "Multiplication": {
+                              "add": [0, a],
+                              "remove": [0, a],
+                              "limit": 10
+                        },
+                        "Tanh": {
+                              "add": [0, a],
+                              "remove": [0, a],
+                              "limit": 10
+                        },
+                        // "Sine": {
+                        //       "add": [0, a],
+                        //       "remove": [0, a],
+                        //       "limit": 10
+                        // },
+                        // "Cosine": {
+                        //       "add": [0, a],
+                        //       "remove": [0, a],
+                        //       "limit": 10
+                        // },
+                        // "Abs": {
+                        //       "add": [0, a],
+                        //       "remove": [0, a],
+                        //       "limit": 10
+                        // }
+                  },
+                  "connections": {
+                        "add": [0, 5],
+                        "remove": [0, 5],
+                        "limit": 5000,
+                        "init": [-1, 1],
+                        "value": {
+                              "mutation_rate": 1,
+                              "mutation_size": 0.001,
+                        }
+                  }
+            },
+            "update": update_settings,
+            "log": true,
+            "return": "network"
+      });
+
+      document.getElementById("output").innerText = predict("2+ 2=5");
+}
+
+setInterval(update, 1);
