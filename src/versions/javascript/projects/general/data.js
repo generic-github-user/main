3a2c0c25ea91d41ee98fe70b9fc5bed3ed314484
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-11-18 11:31:09 -0500

    Create data.js

---
+++ src/versions/javascript/projects/general/data.js
@@ -0,0 +1,14 @@
+var data = [{
+            "input": "5+5",
+            "output": "10"
+      },
+      {
+            "input": "2*3",
+            "output": "6"
+      },
+      {
+            "input": "what is 5 times 20",
+            "output": "100"
+      },
+
+];
