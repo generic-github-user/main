63ac03efea1716e621d0e5725ae06bbeef4b079e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-11-19 21:02:57 -0500

    Moved string encoding and decoding functions to main library file

--- src/versions/javascript/library/caesium.js
+++ src/versions/javascript/library/caesium.js
@@ -176,6 +176,72 @@ cs.apply = function(array, func) {
       return array.map(x => func(x));
 }
 
+cs.encode = {};
+cs.decode = {};
+
+cs.encode.integer = function(input, charset, length) {
+      var output = [];
+      for (var i = 0; i < length; i++) {
+            output.push(charset.indexOf(input[i]));
+      }
+      return output;
+}
+
+cs.decode.integer = function(input, charset) {
+      var output = "";
+      for (var i = 0; i < input.length; i++) {
+            var character = charset[input[i]];
+            if (!character) {
+                  character = "";
+            }
+            output += character;
+      }
+      return output;
+}
+
+// Converting a string to and from a one-hot encoded binary vector
+
+// Accepts a string and returns an array of 1s and 0s
+cs.encode.one_hot = function(input, charset, length) {
+      // Define output as a variable containing an empty array
+      var output = [];
+      // Convert input string to array of all characters in input
+      input = input.split("");
+      // Loop through each character of the input string
+      for (var i = 0; i < length; i++) {
+            // Loop through each possible character
+            charset.forEach(
+                  char => {
+                        // Check for a match between the current input character and the current character from the list; if there is a match, add a 1 to the output array
+                        if (input[i] == char) {
+                              output.push(1);
+                        }
+                        // If there is no match, add a 0 to the output array
+                        else {
+                              output.push(0);
+                        }
+                  }
+            )
+      }
+      // Return output array
+      return output;
+}
+
+// Accepts an array of 1s and 0s as an input and returns a string
+cs.decode.one_hot = function(input, charset) {
+      // Define output as a variable containing an empty string
+      var output = "";
+      // Loop through each character of the input string
+      for (var i = 0; i < input.length; i++) {
+            // Check value of current element of input array
+            if (input[i] == 1) {
+                  output += charset[i % charset.length];
+            }
+      }
+      // Return output string
+      return output;
+}
+
 cs.node_types = function(name) {
       // Log error message to console
       console.error("Node type " + name + " does not exist. Please select a node type from the list of supported node types.");
