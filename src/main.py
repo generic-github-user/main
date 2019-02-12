e280e8b9ed54a479975cc2a2e80b1d26f005e9b0
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-12 13:36:47 -0500

    Convert image data to tensor

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ for filename in glob.glob('../data/*.ico'):
       img = img.resize((32, 32))
       icons.append(list(sum(list(img.getdata()), ())))
 
+data = tf.constant(icons, dtype=tf.float32)
 
 inputs = tf.keras.Input(shape=(4096,))
 a = tf.keras.layers.Dense(128, activation=tf.nn.sigmoid)(inputs)
