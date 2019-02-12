f74209dad599c176a0f19179cd9dc47758857fa7
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-12 11:52:30 -0500

    Add basic TensorFlow autoencoder model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from PIL import Image
 import glob
+import tensorflow as tf
 
 icons = []
 for filename in glob.glob('../data/*.ico'):
       img = Image.open(filename)
       icons.append(img)
+
+inputs = tf.keras.Input(shape=(256,))
+a = tf.keras.layers.Dense(128, activation=tf.nn.sigmoid)(inputs)
+b = tf.keras.layers.Dense(64, activation=tf.nn.sigmoid)(a)
+c = tf.keras.layers.Dense(128, activation=tf.nn.sigmoid)(b)
+outputs = tf.keras.layers.Dense(256, activation=tf.nn.sigmoid)(c)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
