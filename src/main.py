a6b346df033243642cf066aa7be7ee82dca855d7
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-12 16:09:13 -0500

    Add more settings

--- src/main.py
+++ src/main.py
@@ -7,6 +7,9 @@ tf.enable_eager_execution()
 resolution = 32
 channels = 4
 points = (resolution ** 2) * channels
+epochs = 1000
+optimizer = tf.train.AdamOptimizer(0.01)
+delay = 0.1
 
 icons = []
 for filename in glob.glob('../data/*.ico'):
@@ -25,24 +28,24 @@ e = tf.keras.layers.Dense(128, activation=tf.nn.sigmoid)(d)
 outputs = tf.keras.layers.Dense(points)(e)
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-model.compile(optimizer=tf.train.AdamOptimizer(0.01),
+model.compile(optimizer=optimizer,
               loss='mean_squared_error',
               metrics=['accuracy'])
 
-plot = plt.imshow(tf.zeros([resolution, resolution, 4]), interpolation='nearest')
+plot = plt.imshow(tf.zeros([resolution, resolution, channels]), interpolation='nearest')
 plt.ion()
 plt.show()
 class Render(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         image = tf.slice(data, tf.constant([0, 0]), tf.constant([1, points]))
-        prediction = tf.cast(tf.reshape(model(image), [resolution, resolution, 4]), tf.int32)
+        prediction = tf.cast(tf.reshape(model(image), [resolution, resolution, channels]), tf.int32)
         plot.set_data(prediction)
         plt.draw()
-        plt.pause(0.1)
+        plt.pause(delay)
 
 callback = Render()
 
-model.fit(data, data, epochs=1000, callbacks=[callback])
+model.fit(data, data, epochs=epochs, callbacks=[callback])
 model.evaluate(data, data)
 
 print('Press enter to exit the program.')
