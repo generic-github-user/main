4ff5fcd0dfb1c386fbec54a22d84b45319ee4be5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-29 11:34:34 -0500

    loop through and update

--- simulation.py
+++ simulation.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import time
+
+plt.ion()
 
 width = 50
 height = 50
@@ -9,11 +12,19 @@ data = np.zeros([width, height])
 data[width // 2, height // 2] = 1
 
 fig, ax = plt.subplots()
-im = ax.imshow(
-    data,
-    interpolation='nearest',
-    cmap=cm.plasma,
-    origin='lower'
-)
 
-plt.show()
+for u in range(100):
+    for i in range(width):
+        for j in range(height):
+            for q in range(width):
+                for w in range(height):
+                    data[i, j] += data[q, w] * 0.001
+
+    im = ax.imshow(
+        data,
+        interpolation='nearest',
+        cmap=cm.plasma,
+        origin='lower'
+    )
+    plt.pause(1);
+    plt.show()
