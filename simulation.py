10c63c61ca8b57188d152392e0569a4c8c671ce3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-29 12:16:23 -0500

    Fuel generation

--- simulation.py
+++ simulation.py
@@ -7,21 +7,30 @@ plt.ion()
 
 width = 50
 height = 50
-data = np.zeros([width, height])
 
-data[width // 2, height // 2] = 1
+fuel = np.zeros([width, height])
+for i in range(20):
+    fuel[
+        int(np.random.uniform(0, width)): int(np.random.uniform(0, width)),
+        int(np.random.uniform(0, height)): int(np.random.uniform(0, height))
+    ] = np.random.uniform(0.5, 1)
+
+fire = np.zeros([width, height])
+fire[width // 2, height // 2] = 1
+
+render = np.random.uniform(0, 1, [width, height, 3])
 
 fig, ax = plt.subplots()
 
 for u in range(100):
-    for i in range(width):
-        for j in range(height):
-            for q in range(width):
-                for w in range(height):
-                    data[i, j] += data[q, w] * 0.001
+    # for i in range(width):
+    #     for j in range(height):
+    #         for q in range(width):
+    #             for w in range(height):
+    #                 data[i, j] += data[q, w] * 0.001
 
     im = ax.imshow(
-        data,
+        fuel,
         interpolation='nearest',
         cmap=cm.plasma,
         origin='lower'
