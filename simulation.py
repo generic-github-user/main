3b478314190423570dc4416b12ad663afc93a4c6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-29 12:50:07 -0500

    Add comments

--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+# Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -5,9 +6,12 @@ import time
 
 plt.ion()
 
+# Settings
+
 width = 50
 height = 50
 
+# Matrix to store amount of fuel in each cell
 fuel = np.zeros([width, height])
 for i in range(20):
     fuel[
@@ -15,9 +19,11 @@ for i in range(20):
         int(np.random.uniform(0, height)): int(np.random.uniform(0, height))
     ] = np.random.uniform(0.5, 1)
 
+# Store intensity of fire/heat at each cell value
 fire = np.zeros([width, height])
 fire[width // 2, height // 2] = 1
 
+# Rendered output composed from other two main matrices
 render = np.random.uniform(0, 1, [width, height, 3])
 
 fig, ax = plt.subplots()
@@ -29,11 +35,14 @@ for u in range(100):
     #             for w in range(height):
     #                 data[i, j] += data[q, w] * 0.001
 
+    # Update
     im = ax.imshow(
         fuel,
         interpolation='nearest',
         cmap=cm.plasma,
         origin='lower'
     )
-    plt.pause(1);
+    # Wait until next timestep
+    plt.pause(1)
+    # Display updated visualization
     plt.show()
