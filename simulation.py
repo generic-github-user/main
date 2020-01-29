40036c0b79232bfab85403176a707a8b8c687e69
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-29 11:26:16 -0500

    Fix starting conditions

--- simulation.py
+++ simulation.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-data = np.random.uniform(0, 1, [50, 50])
+width = 50
+height = 50
+data = np.zeros([width, height])
+
+data[width // 2, height // 2] = 1
 
 fig, ax = plt.subplots()
 im = ax.imshow(
