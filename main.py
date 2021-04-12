d0ba7a1d51d6d5e028cb77c91162fd80d4a1c2c9
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-12 07:35:02 -0400

    add pen step method

--- main.py
+++ main.py
@@ -4,23 +4,48 @@ import pprint
 
 npa = np.array
 
+# TODO: simulate angle of writing implement
+
 outlines = {
     # 'a': ['left', 'down']
     'a': 'ur,u,l,dl,d,r,ur,dr'
 }
 
 coords = {
-    'u': npa([0, 1]),
-    'd': npa([0, -1]),
+    'u': npa([0, -1]),
+    'd': npa([0, 1]),
     'r': npa([1, 0]),
     'l': npa([-1, 0]),
-    'm': npa([-1, 0]),
+    'm': npa([0, 0]),
 }
 
+def normalize(v):
+    norm=np.linalg.norm(v, ord=1)
+    if norm==0:
+        norm=np.finfo(v.dtype).eps
+    return v/norm
+
 class Pen:
-    def __init__(self, pos, vel):
+    def __init__(self, pos, vel, canvas):
         self.pos = pos
         self.vel = vel
+        self.canvas = canvas
+        self.penSize = 5
+        self.friction = 0.04
+    def step(self, target):
+        # self.vel += np.sqrt((target - self.pos))
+        # self.vel = (target - self.pos) / 1000
+        self.vel += (target - self.pos) / 80 - (self.vel * self.friction)
+        # self.vel += (target - self.pos) / 800 * self.friction
+        # self.vel += (target - self.pos) / 50 - (np.mean(self.vel) * self.friction)
+        # print((target - self.pos))
+        # print(np.sqrt((target - self.pos)))
+        # self.vel += normalize(target - self.pos)
+        self.pos += self.vel * 0.1
+        x, y = np.round(self.pos).astype('int')
+        # print(x, y)
+        v = int(self.penSize / 2)
+        self.canvas[x-v: x+v, y-v: y+v].fill(1)
 
 class Drawer:
     def __init__(self, text, dims=[300, 600], letterSize=30):
