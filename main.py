11f0f4814409da4c70758a4ff40bb7b657b79f45
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-13 13:55:24 -0400

    update function variable names

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import json
 
 def load_data(self, path='cq_data.json'):
-    with open(dictionary, 'r') as dictionary_file:
-        loaded_data = json.loads(dictionary_file.read())
+    with open(path, 'r') as json_file:
+        loaded_data = json.loads(json_file.read())
     return loaded_data
 
 def save_data(self, data, path='cq_data.json'):
-    with open(path, 'w') as savefile:
-        json.dump(data, savefile)
+    with open(path, 'w') as save_file:
+        json.dump(data, save_file)
     return True
