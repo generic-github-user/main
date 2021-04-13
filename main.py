77a7a86a4815cb661fd924fed8208b86d0578bce
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-13 14:26:36 -0400

    remove unused self argument

--- main.py
+++ main.py
@@ -37,12 +37,12 @@ class Task:
     def stringify(self, compressed=True):
         return json.dumps(self.as_dict(compressed))
 
-def load_data(self, path='cq_data.json'):
     with open(path, 'r') as json_file:
         loaded_data = json.loads(json_file.read())
+def load_data(path='cq_data.json'):
     return loaded_data
 
-def save_data(self, data, path='cq_data.json'):
+def save_data(data, path='cq_data.json'):
     with open(path, 'w') as save_file:
         json.dump(data, save_file)
     return True
