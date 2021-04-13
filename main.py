73310a8ba065ce564e56bf958079ce77f055cd1d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-13 14:00:55 -0400

    create Task class

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import json
+import time
+
+class Task:
+    def __init__(self, content, name='', created=None, modified=None):
+        current_time = time.time()
+
+        self.name = name
+        self.content = content
+
+        if created:
+            self.created = created
+        else:
+            self.created = current_time
+
+        if modified:
+            self.modified = modified
+        else:
+            self.modified = current_time
 
 def load_data(self, path='cq_data.json'):
     with open(path, 'r') as json_file:
