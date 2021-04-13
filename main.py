112b310c99e4465a914b625caba882cdc2c26bc1
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-13 13:02:57 -0400

    add functions for loading/saving data

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import json
+
+def load_data(self, path='cq_data.json'):
+    with open(dictionary, 'r') as dictionary_file:
+        loaded_data = json.loads(dictionary_file.read())
+    return loaded_data
+
+def save_data(self, data, path='cq_data.json'):
+    with open(path, 'w') as savefile:
+        json.dump(data, savefile)
+    return True
