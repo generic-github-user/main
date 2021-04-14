63ec9bbf10f45ea4fd3ea5427d996f3ac69734b9
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-14 13:09:46 -0400

    add tag import/export functions (nearly identical to task class methods)

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ class Settings:
     }
     task_properties = ['name', 'content', 'created', 'modified', 'datestring', 'dateparse', 'dateparams', 'datesummary', 'next, id', 'importance']
     task_props_short = ['n', 'c', 'tc', 'tm', 'ds', 'dp', 'dr', 'dv', 'nx', 'i', 'im']
+    tag_properties = ['name', 'description', 'created', 'modified', 'color', 'parent']
+    tag_props_short = ['n', 'd', 'cr', 'm', 'co', 'p']
     command_buffer_size = 20
 
 class Tag:
@@ -42,6 +44,37 @@ class Tag:
             self.modified = modified
         else:
             self.modified = current_time
+    def as_dict(self, compressed=True):
+        task_dict = {}
+        # If 'compressed' is set to True, use the abbreviated tag properties (as listed in settings)
+        if compressed:
+            for i, prop in enumerate(Settings.tag_properties):
+                # Abbreviated attribute name
+                short = Settings.tag_props_short[i]
+                # Only add the property if this tag has it
+                if hasattr(self, prop):
+                    # print(prop)
+                    task_dict[short] = getattr(self, prop)
+        else:
+            for i, prop in enumerate(Settings.tag_properties):
+                short = Settings.tag_props_short[i]
+                if hasattr(self, prop):
+                    task_dict[prop] = getattr(self, prop)
+        return task_dict
+    def stringify(self, compressed=True):
+        # Get dictionary and convert to a string, then return
+        return json.dump(self.as_dict(compressed))
+    def from_dict(self, data, compressed=True):
+        if compressed:
+            for i, prop in enumerate(Settings.tag_properties):
+                short = Settings.tag_props_short[i]
+                # if hasattr(data, short):
+                if short in data:
+                    setattr(self, prop, data[short])
+        else:
+            pass
+
+        return self
 
 class Task:
     def __init__(self, content='', name='', created=None, modified=None, datestring=None, importance=1000):
