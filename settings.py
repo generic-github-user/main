68afb4aa1f59c6597185f972f962aa3664bfc873
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-14 19:52:04 -0400

    fix bug and add more task properties

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ class Settings:
     markers = {
         'dates': '<>'
     }
-    task_properties = ['name', 'content', 'created', 'modified', 'datestring', 'dateparse', 'dateparams', 'datesummary', 'next, id', 'importance']
-    task_props_short = ['n', 'c', 'tc', 'tm', 'ds', 'dp', 'dr', 'dv', 'nx', 'i', 'im']
+    task_properties = ['name', 'content', 'created', 'modified', 'datestring', 'dateparse', 'dateparams', 'datesummary', 'next', 'id', 'importance', 'durationstring', 'duration']
+    task_props_short = ['n', 'c', 'tc', 'tm', 'ds', 'dp', 'dr', 'dv', 'nx', 'i', 'im', 'ds_', 'dp_']
     tag_properties = ['name', 'description', 'created', 'modified', 'color', 'parent']
     tag_props_short = ['n', 'd', 'cr', 'm', 'co', 'p']
     command_buffer_size = 20
