b309834566c9f98663262835a472d7177a217462
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-14 19:50:22 -0400

    add duration parser (...hh:mm:ss)

--- duration.py
+++ duration.py
@@ -7,3 +7,10 @@ time_info = [
     [3600*24*30, 'month', 'mon', 'mn'],
     [3600*24*365.25, 'year', 'yr', 'y']
 ]
+
+def parse_duration(duration):
+    result = 0
+    if ':' in duration:
+        parts = duration.split(':')
+        for i, p in enumerate(parts.reverse()):
+            result += float(p) * time_info[i][0]
