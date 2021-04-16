4c41e87e8ef71187c7c17af51fe3822b43680c38
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-16 10:33:03 -0400

    create Chord class

---
+++ chord.py
@@ -0,0 +1,19 @@
+from note import *
+from pitch import *
+
+class Chord:
+    def __init__(self, start, player=None, num=3, offset=2, key=None):
+        self.notes = []
+        self.player = player
+        self.key = key
+
+        if type(start) is Note:
+            start = start.pitch.nat
+        elif type(start) is Pitch:
+            start = start.nat
+
+        for n in range(num):
+            pitch = start + (offset * n)
+            chord_note = Note(Pitch(pitch, ptype='natural'), key=self.key)
+            self.notes.append(chord_note)
+            # step?
