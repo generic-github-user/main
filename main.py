fa02294e1fbd2b74cad559d163528e5a19cd8000
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-15 20:57:55 -0400

    initial testing

---
+++ main.py
@@ -0,0 +1,10 @@
+import playsound
+from pydub import AudioSegment
+import random
+
+import pygame as pg
+import time
+import pygame.midi
+
+audio_path = './Middle_C.mp3'
+# playsound.playsound(audio_path)
