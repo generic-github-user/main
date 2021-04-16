8358eaf4e43bfc359602265cf1a7e47535af76f6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-16 09:02:04 -0400

    add nesting of melodies (i.e., melody > melodies > notes)

--- melody.py
+++ melody.py
@@ -5,17 +5,18 @@ from pitch import *
 from note import *
 
 class Melody:
-    def __init__(self):
-        self.notes = []
+    def __init__(self, seq=[]):
+        self.sequence = seq
 
     def randomize(self, length, note_length=(1/16,1/4)):
         for n in range(length):
             # next_note = Note(random.choice(naturals))
             next_note = Note(Pitch(random.randint(40, 60), ptype='natural'), length=random.uniform(*note_length))
-            self.notes.append(next_note)
+            self.sequence.append(next_note)
         return self
 
     def play(self, player, tempo=120):
-        for note in self.notes:
-            note.play(player)
-            time.sleep(note.seconds(tempo))
+        for part in self.sequence:
+            part.play(player)
+            if type(part) is Note:
+                time.sleep(part.seconds(tempo))
