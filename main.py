6fc55482319b67a75f81828504cdc2d414d5b3b1
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-15 21:01:55 -0400

    testing pygame sound module

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import pygame.midi
 
 audio_path = './Middle_C.mp3'
 # playsound.playsound(audio_path)
+
+pg.mixer.init()
+pg.init()
+
 notes = ['c', 'd', 'a']
 sounds = []
 for i in range(2):
@@ -16,3 +20,27 @@ for i in range(2):
     # playsound.playsound(note_path, False)
 
     sounds.append(pg.mixer.Sound(note_path))
+
+pg.mixer.set_num_channels(20)
+
+
+# for s in sounds:
+#     s.play()
+
+# input()
+
+
+# pg.midi.init()
+# player = pg.midi.Output(0)
+# player.set_instrument(0)
+#
+# player.note_on(60, 127)
+# player.note_on(64, 127)
+# player.note_on(67, 127)
+#
+# time.sleep(10)
+#
+# player.note_off(64, 127)
+#
+# del player
+# pg.midi.quit()
