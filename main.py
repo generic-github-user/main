25b669252383a33e651a2f97b375ad529cefdbf2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-16 08:06:40 -0400

    add comments to Note class

--- main.py
+++ main.py
@@ -107,17 +107,24 @@ class Note:
         self.length = length
         self.player = player
     def seconds(self, tempo):
+        # beats * (beats / minute) = beats * beats / 60 seconds
         return self.length * (tempo / 60)
     def play(self):
+        # Turn on note using midi pitch and velocity
         self.player.note_on(self.pitch.midi, self.velocity)
     def update_key(self, key):
         self.key = key
+        # Extract naturals from key
         self.key_notes = [n[0] for n in self.key]
 
         natural = self.pitch.natural
         # print(natural)
+
+        # Natural is listed in key signature
         if natural in self.key_notes:
+            # Get corresponding accidental
             acc = self.key[self.key_notes.index(natural)][1]
+            # Shift pitch accordingly
             if acc == '_':
                 self.pitch.step(-0.5)
             elif acc == '^' or acc == '#':
