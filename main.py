e398c6f0ba8d6d90d343c82d0e485e1402d2d004
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-15 21:01:06 -0400

    testing with prerecorded notes

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ import pygame.midi
 
 audio_path = './Middle_C.mp3'
 # playsound.playsound(audio_path)
+notes = ['c', 'd', 'a']
+sounds = []
+for i in range(2):
+    note = random.choice(notes)
+    note_path = './piano-{}_{}_major.wav'.format(note, note.upper())
+    # playsound.playsound(note_path, False)
+
+    sounds.append(pg.mixer.Sound(note_path))
