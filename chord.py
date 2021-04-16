dc40037f8247941df5737b2d83ba76668c98cff9
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-16 14:42:27 -0400

    add flexible chord construction (via Python-style interval or custom indices)

--- chord.py
+++ chord.py
@@ -4,7 +4,7 @@ from note import *
 from pitch import *
 
 class Chord:
-    def __init__(self, start, player=None, num=3, offset=2, key=None, length=1):
+    def __init__(self, start, player=None, num=3, offset=2, key=None, length=1, interval=None, custom=None):
         self.notes = []
         self.player = player
         self.key = key
@@ -15,14 +15,27 @@ class Chord:
         elif type(start) is Pitch:
             start = start.nat
         elif type(start) is str:
-            start = Note(start)
+            start = Note(start, ptype='natural')
             start = start.pitch.nat
 
-        for n in range(num):
-            pitch = start + (offset * n)
-            chord_note = Note(Pitch(pitch, ptype='natural'), key=self.key)
-            self.notes.append(chord_note)
-            # step?
+        if custom:
+            for b in custom:
+                pitch = start + b
+                chord_note = Note(Pitch(pitch, ptype='natural'), key=self.key)
+                self.notes.append(chord_note)
+                # TODO
+        elif interval:
+            x, y, z = interval
+            for c in range(x, y+1, z):
+                pitch = start + c
+                chord_note = Note(Pitch(pitch, ptype='natural'), key=self.key)
+                self.notes.append(chord_note)
+        elif num:
+            for n in range(num):
+                pitch = start + (offset * n)
+                chord_note = Note(Pitch(pitch, ptype='natural'), key=self.key)
+                self.notes.append(chord_note)
+                # step?
 
     def play(self, player=None):
         if player:
@@ -31,3 +44,5 @@ class Chord:
             note.play(player=self.player)
         # TODO:
         time.sleep(self.length)
+
+        return self
