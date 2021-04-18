17a89c535c1bb7c0551f43c87ca9633894855704
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-17 22:44:50 -0400

    refactoring

--- scale.py
+++ scale.py
@@ -5,7 +5,7 @@ from melody import *
 class Scale(Melody):
     def __init__(self, start, steps, velocity=127, note_length=1/4, use_chord=False, chord_size=3, play=False, skip=1, key=None, player=None):
         """Generate a scale given a starting point, number of steps, and information about each note"""
-        
+
         assert key is not None
         assert player is not None
         self.key = key
@@ -22,30 +22,13 @@ class Scale(Melody):
 
         start = start.pitch.nat
 
+        for i in list(chain(range(0, steps, skip), range(steps, -1, -skip))):
+            if use_chord:
+                scale_note = Chord(Pitch(start+i,ptype='natural'), player=self.p, key=self.key, length=note_length, num=chord_size)
+            else:
+                scale_note = Note(Pitch(start+i, ptype='natural'), player=self.p, key=self.key, length=note_length)
+
+            self.add(scale_note)
+
         if play:
-            # for i in [range(0, steps) + range(steps, 0)]:
-            # print(list(chain(range(0, steps), range(steps, 0, -1))))
-            # s = steps + 1
-            for i in list(chain(range(0, steps, skip), range(steps, -1, -skip))):
-                if use_chord:
-                    # self.chord(start+i, num=chord_size)
-
-                    nnote = Chord(Pitch(start+i, ptype='natural'), player=self.p, key=self.key, length=note_length, num=chord_size)
-                    # self.play(nnote)
-                    nnote.play()
-                else:
-                    # self.player.note_on(self.adjust_pitch(start+i), velocity)
-                    # print(start+i)
-                    # self.play_note(pitch=start+i)
-                    # self.play_note(note=Note(Pitch(start+i,type='natural')))
-
-                    nnote = Note(Pitch(start+i, ptype='natural'), self.player, key=self.key)
-                    self.play(nnote)
-        else:
-            for i in list(chain(range(0, steps, skip), range(steps, -1, -skip))):
-                if use_chord:
-                    scale_note = Chord(Pitch(start+i,ptype='natural'), player=self.p, key=self.key, length=note_length, num=chord_size)
-                else:
-                    scale_note = Note(Pitch(start+i, ptype='natural'), player=self.p, key=self.key, length=note_length)
-
-                self.add(scale_note)
+            self.play()
