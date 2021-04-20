8330ec2881e23ea63e689f338a087f779a40ac2f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-19 21:36:40 -0400

    init and add RowGame class

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class RowGame:
+    def __init__(self, m=3, k=3, n=3, r=1):
+        self.m = m
+        self.k = k
+        self.n = n
+        self.r = r
+        self.board = np.zeros([m, k])
