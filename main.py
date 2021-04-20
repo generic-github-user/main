56cdf89bb9f927781d71ff524f32e4eb9f516a18
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-19 21:55:29 -0400

    add method to check for wins in rows/columns of a game board

--- main.py
+++ main.py
@@ -15,3 +15,27 @@ class RowGame:
 
     def clone(self):
         return copy.deepcopy(self)
+
+    # def checkDim(self):
+
+    def checkArray(self, grid):
+        for row in grid:
+            player_streak = 0
+            streak_len = 0
+            for x in row:
+                if x == player_streak:
+                    streak_len += 1
+                else:
+                    streak_len = 0
+                    player_streak = x
+
+                if streak_len >= self.n and player_streak != 0:
+                    return player_streak
+        return player_streak
+
+    def check(self):
+        for g in [self.board, self.board.transpose()]:
+            winner = self.checkArray(g)
+            if winner != 0:
+                return winner
+        return 0
