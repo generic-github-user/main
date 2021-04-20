08f294dc7604a25f0594f050b4c44d18c5f59620
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-19 22:26:15 -0400

    add comments

--- main.py
+++ main.py
@@ -27,18 +27,23 @@ class RowGame:
 
     def print(self, type='normal'):
         if type == 'normal':
+            # Loop through rows
             for row in self.board:
+                # Generate and print string representing row of game board
                 row_string = ' '.join([self.cellSym(col) for col in row])
                 print(row_string)
         elif type == 'raw':
+            # Print plain NumPy array
             print(self.board)
 
     # def checkDim(self):
 
     def checkArray(self, grid):
         for row in grid:
+            # Track the player who made the current move(s)
             player_streak = 0
             streak_len = 0
+            # Loop through each mark in row (after transformation; so the "row" might actually be a column or diagonal)
             for x in row:
                 if x == player_streak:
                     streak_len += 1
@@ -48,17 +53,20 @@ class RowGame:
 
                 if streak_len >= self.n and player_streak != 0:
                     return player_streak
+        # If no player won in any of the checked rows, return 0
         return 0
 
     def diagonalize(self, grid):
         x, y = grid.shape
         result = []
+        # Loop through diagonal offsets; a [3, 5] array will have offsets from -5 to 3
         for d in range(-y, x):
             result.append(grid.diagonal(d))
         return result
 
     def check(self):
         d_ = self.diagonalize
+        # Loop through rows, columns, and both diagonals
         for g in [self.board, self.board.transpose(), d_(self.board), d_(self.board.transpose())]:
             winner = self.checkArray(g)
             if winner != 0:
