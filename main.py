1edcb093d06297efc1369f23c8547231cd2a2ef2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-19 21:55:09 -0400

    add player class (might not be used)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import copy
 
+class Player:
+    def __init__(self, name):
+        self.name = name
+
 class RowGame:
     def __init__(self, m=3, k=3, n=3, r=1):
         self.m = m
