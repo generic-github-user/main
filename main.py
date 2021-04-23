32a3608859ef8a1314f33c60424253ffe0e0ffcc
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-22 21:48:34 -0400

    add point rotation functions (https://gist.github.com/LyleScott/e36e08bfb23b1f87af68c9051f985302)

--- main.py
+++ main.py
@@ -45,3 +45,60 @@ def weighted_line(r0, c0, r1, c1, w, rmin=0, rmax=np.inf):
     mask = np.logical_and.reduce((yy >= rmin, yy < rmax, vals > 0))
 
     return (yy[mask].astype(int), xx[mask].astype(int), vals[mask])
+
+# Point rotation functions credit:
+# Lyle Scott, III  // lyle@ls3.io
+# https://gist.github.com/LyleScott/e36e08bfb23b1f87af68c9051f985302
+
+def rotate_via_numpy(xy, radians):
+    """Use numpy to build a rotation matrix and take the dot product."""
+    x, y = xy
+    c, s = np.cos(radians), np.sin(radians)
+    j = np.matrix([[c, s], [-s, c]])
+    m = np.dot(j, [x, y])
+
+    return float(m.T[0]), float(m.T[1])
+
+
+def rotate_origin_only(xy, radians):
+    """Only rotate a point around the origin (0, 0)."""
+    x, y = xy
+    xx = x * math.cos(radians) + y * math.sin(radians)
+    yy = -x * math.sin(radians) + y * math.cos(radians)
+
+    return xx, yy
+
+
+def rotate_around_point_lowperf(point, radians, origin=(0, 0)):
+    """Rotate a point around a given point.
+
+    I call this the "low performance" version since it's recalculating
+    the same values more than once [cos(radians), sin(radians), x-ox, y-oy).
+    It's more readable than the next function, though.
+    """
+    x, y = point
+    ox, oy = origin
+
+    qx = ox + math.cos(radians) * (x - ox) + math.sin(radians) * (y - oy)
+    qy = oy + -math.sin(radians) * (x - ox) + math.cos(radians) * (y - oy)
+
+    return qx, qy
+
+
+def rotate_around_point_highperf(xy, radians, origin=(0, 0)):
+    """Rotate a point around a given point.
+
+    I call this the "high performance" version since we're caching some
+    values that are needed >1 time. It's less readable than the previous
+    function but it's faster.
+    """
+    x, y = xy
+    offset_x, offset_y = origin
+    adjusted_x = (x - offset_x)
+    adjusted_y = (y - offset_y)
+    cos_rad = math.cos(radians)
+    sin_rad = math.sin(radians)
+    qx = offset_x + cos_rad * adjusted_x + sin_rad * adjusted_y
+    qy = offset_y + -sin_rad * adjusted_x + cos_rad * adjusted_y
+
+    return qx, qy
