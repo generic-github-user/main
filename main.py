38590db6425407ad0e5903339da57902c89b66f9
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-30 17:23:36 -0400

    add dimension inference from cell width and world size

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Automata:
     def __init__(self, size=None):
         if size is None:
             size = [64, 64]
-        self.size = size
+        self.size = np.array(size)
         self.cell_width = 10
         self.world = np.random.randint(0, 2, self.size)
         # self.world = np.zeros(self.size)
@@ -40,11 +40,14 @@ class Scene:
         self.root = Tk()
         # root.title()
         # root.resizable(False, False)
-        self.canvas = Canvas(self.root, width = 400, height = 400)
-        self.canvas.pack()
         self.content = Automata(size=[30, 30])
-    def step(self, i=0, n=20):
+        self.dimensions = self.content.size * self.content.cell_width
+        w, h = self.dimensions
+        self.canvas = Canvas(self.root, width = w, height = h)
+        self.canvas.pack()
+    def step(self, i=0, n=20, render=True):
         if i < n:
+            self.content.evolve()
             # self.canvas.create_rectangle(20, 20, 50, 50, fill='red')
             world = self.content.world
             self.canvas.delete('all')
