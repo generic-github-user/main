283a4d387dabf61a803e573dd08e53bd2c99cc3c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-05-02 19:07:02 -0400

    clean up template path definition

--- doc-generator.py
+++ doc-generator.py
@@ -4,16 +4,19 @@ import datetime
 module_name = 'main'
 docs_directory = './docs'
 d_ = docs_directory
-template = d_+'/class_template.md'
 output = d_+'/main.md'
 result = ''
-method_template = d_+'/method_template.md'
 
-with open(template, 'r') as file:
-    template_content = file.read()
-    # print(template_content)
-with open(method_template, 'r') as file:
-    method_template = file.read()
+templates = {
+    'class': 'class_template.md',
+    'method': 'method_template.md',
+    'parameter': 'parameter_template.md'
+}
+template_content = {}
+for k, v in templates.items():
+    path = d_+'/'+v
+    with open(path, 'r') as file:
+        template_content[k] = file.read()
 
 def indent_width(s):
     indent = len(s) - len(s.lstrip())
