906e8de90675ccccff067b00fd3a83a320d7cc5c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-05-02 16:29:23 -0400

    add doc gen settings

--- doc-generator.py
+++ doc-generator.py
@@ -1,2 +1,10 @@
 import importlib, inspect
 import datetime
+
+module_name = 'main'
+docs_directory = './docs'
+d_ = docs_directory
+template = d_+'/class_template.md'
+output = d_+'/main.md'
+result = ''
+method_template = d_+'/method_template.md'
