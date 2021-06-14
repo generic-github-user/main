0085b8ab5516e8f6156c4ec0c46ea4bb12f03361
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-14 03:28:42 -0400

    add improved tag parser (this was written a while ago, is somewhat messy, and will most likely be replaced)

--- doc-generator.py
+++ doc-generator.py
@@ -1,5 +1,7 @@
 import importlib, inspect
 import datetime
+import json
+import random
 
 module_name = 'main'
 docs_directory = './docs'
@@ -97,6 +99,46 @@ def split_numeric(self, text, parse=True):
         block = float(block)
     output.append(block)
     return output
+
+data_types = ['int', 'str', 'float', 'bool', 'func', 'array']
+def parse_tags(tag_list):
+    result = '**'
+    for i, t in enumerate(tag_list):
+        if i < len(tag_list) - 1:
+            next = tag_list[i+1]
+        else:
+            next = None
+
+        # try:
+        #     next = tag_list[i+1]
+        # except:
+        #     pass
+
+        # if type(t) is list:
+        #     parse_tags(t)
+
+        if t in data_types:
+            result += '`{}`'.format(t)
+            if type(next) is list:
+                if t in ['str']:
+                    result += ' in `{}`'.format(', '.join(next))
+                    tag_list.remove(next)
+                elif t in ['func']:
+                    result += ' (`{}` -> `{}`)'.format(*next)
+                    tag_list.remove(next)
+        elif '-' in str(t):
+            limits = t.split('-')
+            result += ' between `{}` and `{}`'.format(*limits)
+        elif type(t) is not list and t in symbols:
+            # elif any(s in t for s in symbols):
+            result += ' {} {}'.format(symbols[t], next)
+        elif type(t) is list and t[0] in data_types:
+            if type(t[1]) in [int, float, str]:
+                t[1] = [t[1]]
+            result += '`{}` array of shape `{}`'.format(*t)
+    result += '**'
+    return result
+
 def generate_section(stype, object, replacements):
     docstring = object.__doc__
     if docstring is None:
