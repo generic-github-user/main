07b11ab1532246a85466603757b41366666b3ab2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-18 22:06:00 -0400

    add imports

--- caterpillar.py
+++ caterpillar.py
@@ -1,1 +1,5 @@
 # The following code is intended solely for educational and experimental purposes. I do not condone any unethical use of the code and disclaim responsibility from such use.
+
+import ast
+import random
+import operator as ops
