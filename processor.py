74102bbfe95b4466de95e8fd717a248ed593b576
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-26 21:32:39 -0400

    Add test statements

--- processor.py
+++ processor.py
@@ -8,3 +8,24 @@ import ast
 import re
 import string
 import time
+
+
+# This is a simple Python preprocessor designed to improve concision when using some common operators and functions. It parses the file with a series of regular expressions and replaces abbreviations and various syntactical sugar with their normal Python formats. The tweaks are mostly centered around data access and operations involving higher-order functions.
+
+# In[91]:
+
+
+sample = """
+[52]**3
+[6]***4
+xy::xy+5
+5~[3,3,3]
+4:90
+1:100:5
+dictionary..key
+a..b..c
+F$G
+A$B$C$x
+F^6(x)
+C ++
+"""
