36fefddc505853662f733c690f65a59771c6ed5a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-27 01:10:29 -0400

    support application of iterable transform to list (e.g., of words in input string) instead of only text

--- automacro.py
+++ automacro.py
@@ -16,11 +16,11 @@ examples = [
 ]
 manipulations = [
     [lambda x: x[0], 'First char'],
-    [lambda x: x[::-1], 'Reverse'],
+    [lambda x: x[::-1], 'Reverse', True],
     [str.lower, 'Lowercase'],
     [str.upper, 'Capitalize'],
     [str.split, 'Split'],
-    [lambda x: ''.join(x), 'Join'],
+    [lambda x: ', '.join(x), '', True]
 ]
 M = [m[0] for m in manipulations]
 options = []
@@ -28,16 +28,23 @@ max_ops = 3
 print(f'Testing {max_ops**len(manipulations)} operations across {len(examples)} examples')
 checked = 0
 for n in range(max_ops):
-    for i in itertools.product(M, repeat=n+1):
+    print((n+1)**len(manipulations))
+    for i in itertools.product(manipulations, repeat=n+1):
     #     sequence = []
         for ex in examples:
     #         print('Testing sample {} -> {}'.format(*ex))
             S = ex[0]
             for j in i:
+#                 print(S, len(j))
+                f = j[0]
                 if type(S) is str:
-                    S = j(S)
+                    S = f(S)
                 elif type(S) in [list, tuple]:
-                    S = list(map(j, S))
+                    if len(j) < 3:
+                        S = list(map(f, S))
+                    elif j[-1] == True:
+                        S = f(S)
+                        
                     if type(S[0]) is list:
                         S = list(itertools.chain(*S))
                 else:
@@ -55,4 +62,17 @@ for n in range(max_ops):
     
 print(f'Checked {checked} combinations and found {len(options)} matches')
 print(options)
+# TODO: add heuristics (e.g., scoring intermediate results based on desirable properties of the generated string)
+
+
+# In[111]:
+
+
+manipulations[8][0](list('test'))
+
+
+# In[ ]:
+
+
+
 
