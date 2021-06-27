4d78d275a331e9ca4747dc833110e4ccad4c2634
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-27 00:02:21 -0400

    Search combinations of varying lengths (1 to max)

--- automacro.py
+++ automacro.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[47]:
+# In[56]:
 
 
 import itertools
@@ -20,39 +20,35 @@ manipulations = [
     [str.split, 'Split'],
     [lambda x: ''.join(x), 'Join'],
 ]
+M = [m[0] for m in manipulations]
 options = []
 max_ops = 3
 print(f'Testing {max_ops**len(manipulations)} operations across {len(examples)} examples')
-for i in itertools.product(manipulations, repeat=max_ops):
-#     sequence = []
-    for ex in examples:
-#         print('Testing sample {} -> {}'.format(*ex))
-        S = ex[0]
-        for j in i:
-            if type(S) is str:
-                S = j(S)
-            elif type(S) in [list, tuple]:
-                S = list(map(j, S))
-                if type(S[0]) is list:
-                    S = list(itertools.chain(*S))
-            else:
-                print(type(S))
-        
-        if type(S) is list:
-            S = ''.join(S)
-        if S != ex[1]:
-#             print(S)
-            break
-    else:
-        print(S, True)
-        options.append(i)
+for n in range(max_ops):
+    for i in itertools.product(M, repeat=n+1):
+    #     sequence = []
+        for ex in examples:
+    #         print('Testing sample {} -> {}'.format(*ex))
+            S = ex[0]
+            for j in i:
+                if type(S) is str:
+                    S = j(S)
+                elif type(S) in [list, tuple]:
+                    S = list(map(j, S))
+                    if type(S[0]) is list:
+                        S = list(itertools.chain(*S))
+                else:
+                    print(type(S))
+
+            if type(S) is list:
+                S = ''.join(S)
+            if S != ex[1]:
+    #             print(S)
+                break
+        else:
+            print(S, True)
+            options.append(i)
     
     
 print(options)
-
-
-# In[ ]:
-
-
-
 
