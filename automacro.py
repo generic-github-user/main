bf339eb1b284b938f4f142e2b7bbd514e5f6f37f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-26 23:49:18 -0400

    add example string mappings

--- automacro.py
+++ automacro.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[38]:
+# In[47]:
 
 
 import itertools
+
+examples = [
+    ['United States', 'US'],
+    ['Continuous Integration', 'CI'],
+    ['artificial intelligence', 'AI'],
+    ['Machine learning', 'ML']
+]
 manipulations = [
     lambda x: x[0],
     lambda x: x[::-1],
