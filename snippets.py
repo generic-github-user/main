36075321d74c3c60f40e67b5f8bfb6de4c2be3a0
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-30 12:12:50 -0400

    Add nested list flattening function
    See https://stackoverflow.com/a/14491059/10940584 for original code

--- snippets.py
+++ snippets.py
@@ -14,3 +14,22 @@ for i in range(2, 50):
             break
     else:
         print(i)
+
+
+# ### Flatten Nested List
+# 
+# Convert an irregular ordered list of lists and/or values to a shallow sequence of values.
+# 
+# Source: https://stackoverflow.com/a/14491059/10940584
+
+# In[3]:
+
+
+def flatten(L):
+    for item in L:
+        try:
+            yield from flatten(item)
+        except TypeError:
+            yield item
+
+list(flatten([[[1, 2, 3], [4, 5]], 6]))
