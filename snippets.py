cbcd32ecc5450489ea180f5e94833dd4f00d8f40
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-30 12:21:16 -0400

    add function for generating text list from list of items

--- snippets.py
+++ snippets.py
@@ -46,3 +46,12 @@ def title(text, ignore=['in', 'the', 'of', 'with', 'or', 'and']):
     return ' '.join(w[0].upper()+w[1:] if w not in ignore else w for w in text.split(' '))
 
 title('trends in machine learning')
+
+
+# In[9]:
+
+
+def makelist(items):
+    return ', '.join(items[:-1])+', and '+items[-1]
+
+makelist(list('xyz'))
