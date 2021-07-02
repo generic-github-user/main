7b6accd287344ae06652c52e7295e84e491af8c1
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-01 20:17:43 -0400

    Add introduction

---
+++ puzzles.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Programming Puzzles
+# 
+# This is a list of (hopefully interesting) programming puzzles I created (mainly for my own enrichment) with solutions and brief explanations. I didn't want to place any unnecessary restrictions on how the problems are to be approached, but there will be some occasional suggestions about possible variants you might want to try (e.g., finding an answer with a certain time/space complexity, using a certain data structure, etc). Pull requests with better or more interesting solutions, as well as new problems, are welcome.
