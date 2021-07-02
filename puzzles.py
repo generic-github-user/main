4fe4fc5dbb35e0964b640f975e331a982fa3fe96
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-02 12:15:03 -0400

    testing

--- puzzles.py
+++ puzzles.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[142]:
+
+
+get_ipython().system('jupyter nbconvert --to script puzzles.ipynb')
+
+
 # # Programming Puzzles
 # 
 # This is a list of (hopefully interesting) programming puzzles I created (mainly for my own enrichment) with solutions and brief explanations. I didn't want to place any unnecessary restrictions on how the problems are to be approached, but there will be some occasional suggestions about possible variants you might want to try (e.g., finding an answer with a certain time/space complexity, using a certain data structure, etc). Pull requests with better or more interesting solutions, as well as new problems, are welcome.
@@ -174,6 +180,21 @@ fig, ax = plt.subplots()
 ax.plot(output)
 # ax.set_yscale('log')
 print(sum(output))
+
+
+# In[138]:
+
+
+math.log10(math.factorial(32586))
+
+
+# In[127]:
+
+
+list(previous.items())[:100]
+len(list(previous.items()))
+
+
 # ## Automated Logical Induction
 # 
 # Compose a short heuristic program that, given a series of logical statements, can infer logical conclusions.
