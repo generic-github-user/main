1c244eae0f18b8497cca49e15faffedd90fb83e5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-01 20:26:43 -0400

    Add puzzle (and first attempt at solving)

--- puzzles.py
+++ puzzles.py
@@ -4,3 +4,18 @@
 # # Programming Puzzles
 # 
 # This is a list of (hopefully interesting) programming puzzles I created (mainly for my own enrichment) with solutions and brief explanations. I didn't want to place any unnecessary restrictions on how the problems are to be approached, but there will be some occasional suggestions about possible variants you might want to try (e.g., finding an answer with a certain time/space complexity, using a certain data structure, etc). Pull requests with better or more interesting solutions, as well as new problems, are welcome.
+
+# Find the smallest palindromic positive integer greater than 100,000 which contains, in its sequence of digits, the floor of its own square root.
+
+# In[1]:
+
+
+base = ''
+for d in range(4):
+    for i in range(10):
+        s = str(i)
+        num = s+base+s
+        root = int(num) ** (1/2)
+        if str(root) in str(num):
+            print(num)
+            break
