2fce43512f80f380f0721c285d256c61e8a0ede2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-05 00:07:39 -0400

    get list of repositories using GitHub REST API

--- summarize.py
+++ summarize.py
@@ -10,3 +10,17 @@ import time
 import json
 
 from IPython.display import display, Markdown, JSON
+
+
+# In[61]:
+
+
+response = []
+for p in range(1, 6):
+    query = 'https://api.github.com/users/generic-github-user/repos?page='+str(p)
+    data = requests.get(query).json()
+#     print(data)
+    response.extend(data)
+
+print(len(response))
+JSON(response[:5])
