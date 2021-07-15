18abf7fcaa1bb8d811f9f5164c87b753a3dc5676
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-15 13:47:30 -0400

    Add list of relative coordinates for adjacent cells

--- simulation.py
+++ simulation.py
@@ -7,3 +7,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+
+
+# In[12]:
+
+
+# offsets = list(itertools.product([-1, 0, 1], repeat=2))
+offsets = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+offsets = list(map(np.array, offsets))
+print(offsets)
