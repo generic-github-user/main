e43c4a46502372fe4084df994aa1097c1a97463b
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-16 15:42:30 -0400

    Create giraffe.py

---
+++ giraffe.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[108]:
+
+
+import nltk
+from fuzzywuzzy import fuzz
+import string
+import random
+import itertools
+import pprint
+import pyvis
+from IPython.display import JSON
