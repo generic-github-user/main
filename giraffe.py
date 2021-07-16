482dd0e545289f3bc0bd54ff208992c70f220a2b
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-16 15:44:14 -0400

    Load and process text corpus

--- giraffe.py
+++ giraffe.py
@@ -12,3 +12,15 @@ import itertools
 import pprint
 import pyvis
 from IPython.display import JSON
+
+
+# In[8]:
+
+
+hamlet = nltk.corpus.gutenberg.sents('shakespeare-hamlet.txt')
+def words_to_sent(w):
+    s = w[0]
+    s += ''.join([t if t in string.punctuation else ' '+t for t in w[1:]])
+    return s
+hamlet = list(map(words_to_sent, hamlet))
+hamlet = list(filter(lambda l: len(l) >= 10, hamlet))
