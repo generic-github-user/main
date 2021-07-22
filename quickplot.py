b584639b3d9bbe23cba7727fefd0f6a31ab6831c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-21 21:15:14 -0400

    Add project summary

---
+++ quickplot.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# QuickPlot contains cleaner and more coherent versions of some functions I commonly use for rapid multidimensional data visualization based on Matplotlib/Pyplot. The idea is to be able to display most types of data with 1-2 lines of code by handling boilerplate and automatically inferring which strategy is preferred for plotting a specific dataset; along with providing an interface to customize the generated plots at multiple levels of abstraction. The tools are mainly NumPy-focused and I am writing this mainly for my own use, so support/stability is not guaranteed.
