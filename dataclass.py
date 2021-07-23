79e28cd982bff6643ac5f97f315e2bb24c86dfab
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-22 23:15:47 -0400

    add project introduction

---
+++ dataclass.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Punchcard
+
+# ## Introduction
+
+# This toolkit provides flexible and efficient data structures that abstract common manipulations, providing mappings, serialization, restructuring, convenience methods/wrappers, memoization, and more. Numerous convenience methods and data processing utilities are integrated into the class and universally accessible.
+# 
+# The core motivation is reducing the headaches often faced when iterating (and executing functions) over deeply nested and/or irregular data structures, which often results in boilerplate code and poorly standardized/optimized subroutines. The tools included in punchcard aim for brevity, intuitiveness, and developer productivity.
