9607afce8e778cbd4b2ecc554581f3c56595448c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-22 23:24:20 -0400

    Add base imports

--- dataclass.py
+++ dataclass.py
@@ -8,3 +8,13 @@
 # This toolkit provides flexible and efficient data structures that abstract common manipulations, providing mappings, serialization, restructuring, convenience methods/wrappers, memoization, and more. Numerous convenience methods and data processing utilities are integrated into the class and universally accessible.
 # 
 # The core motivation is reducing the headaches often faced when iterating (and executing functions) over deeply nested and/or irregular data structures, which often results in boilerplate code and poorly standardized/optimized subroutines. The tools included in punchcard aim for brevity, intuitiveness, and developer productivity.
+# In[641]:
+
+
+import string
+import operator
+import uuid
+import requests
+import pprint
+import math
+import sys
