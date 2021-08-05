8191407708689f4279edf2cb799e6cb41cfffe03
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-05 01:55:39 -0400

    Add imports

--- symbolic-approximation.py
+++ symbolic-approximation.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# In[989]:
+
+
+import numpy as np
+import operator
+import random
+from IPython.display import Latex, Math
+import IPython.display as display
+import math
+import matplotlib.pyplot as plt
