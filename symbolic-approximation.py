914c5296910ad6d73cd80634be40ab3ba47bcf45
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-05 01:58:27 -0400

    Add base grammars for simple mathematical expressions

--- symbolic-approximation.py
+++ symbolic-approximation.py
@@ -11,3 +11,32 @@ from IPython.display import Latex, Math
 import IPython.display as display
 import math
 import matplotlib.pyplot as plt
+
+
+# In[624]:
+
+
+# Math()
+
+
+# In[2]:
+
+
+X = np.arange(10)
+Y = np.random.randint(0, 20, 10)
+
+
+# In[3935]:
+
+
+# string rewriting?
+expressions = [
+    [['$'], ['F'], 1],
+#     [['$'], ['E']],
+#     [['F'], [[lambda: random.choice(functions[:3])[0], ['E', 'E']]]],
+#     [['F'], [[lambda: random.choice(functions[3:])[0], ['E']]]],
+    [['F'], [['O2', ['E', 'E']]], 3],
+    [['F'], [['O1', ['E']]]],
+#     identity/input
+#     [['$'], ['I']],
+]
