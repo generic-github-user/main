61212b7469aa5a1d999cde53dcdfa67e2ac90be5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-06 23:00:35 -0400

    Add Expression class

--- symbolic-algebra.py
+++ symbolic-algebra.py
@@ -5,3 +5,15 @@
 
 
 import string
+
+# In[217]:
+
+
+class Expression:
+#     def __init__(self, terms=None, *extra_terms):
+    def __init__(self, *terms):
+        if terms is None:
+            terms = []
+#         terms.extend(extra_terms)
+        self.terms = terms
+        self.group = True
