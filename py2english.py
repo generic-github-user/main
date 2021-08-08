322e34967efb9e65bd85e276433a0096341d8408
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-07 22:36:45 -0400

    Add experiment summary

---
+++ py2english.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # py2english
+
+# This program parses a Python program into an abstract syntax tree, then generates a natural language description of what it does and how it does it. This is just an experiment, but if it works well as a proof-of-concept the system could be refined and aid in understanding and analyzing large codebases.
