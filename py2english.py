23fef38879b5506c943e59ca2ab26f54f3c7b4ec
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-07 22:40:57 -0400

    Add sample code snippet (prime number generator)

--- py2english.py
+++ py2english.py
@@ -4,3 +4,21 @@
 # # py2english
 
 # This program parses a Python program into an abstract syntax tree, then generates a natural language description of what it does and how it does it. This is just an experiment, but if it works well as a proof-of-concept the system could be refined and aid in understanding and analyzing large codebases.
+
+# In[1]:
+
+
+import ast
+
+
+# In[2]:
+
+
+sample_code = """
+for i in range(2, 50):
+    for j in range(2, round(i**(1/2))):
+        if i % j == 0:
+            break
+    else:
+        print(i)
+"""
