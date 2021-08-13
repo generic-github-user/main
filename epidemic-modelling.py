239704399ba27c3398249ad9fea092780a60877b
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-13 09:39:24 -0400

    Add Pathogen class

--- epidemic-modelling.py
+++ epidemic-modelling.py
@@ -15,3 +15,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import base64
+
+
+# In[449]:
+
+
+class Pathogen:
+    def __init__(self, **kwargs):
+        defaults = dict(
+            infectiousness=0.01,
+            virulence=0.1,
+            symptomaticity=0.8,
+            incubation_period=14/365,
+            disease_period=7/365,
+        )
+#         period of communicability
+        defaults |= kwargs
+        for k, v in defaults.items():
+            setattr(self, k, v)
