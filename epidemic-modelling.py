c8539afa5d467158694ea2aeaba17eb14417bbe3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-13 09:37:55 -0400

    Create epidemic-modelling.py

---
+++ epidemic-modelling.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# modelling response/policy
+
+
+# In[122]:
+
+
+get_ipython().run_line_magic('matplotlib', 'widget')
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import base64
