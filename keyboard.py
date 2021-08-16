dd8de9e469ebfde82e3fd2e08acb39c70cc18a4e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-16 14:34:27 -0400

    Add function for calculating "effort" needed to type a given string

--- keyboard.py
+++ keyboard.py
@@ -29,3 +29,19 @@ test_strings = [
     '7562855701275165783632875',
     'iuhfunwofinwguibogybnaon'
 ]
+
+def score(text, pointers=5, groups=1, positioning='random', stretch_penalty=0.5, log=False):
+    if positioning == 'random':
+        pos = np.random.randint([0, 0], np.array(layout.shape), (pointers, 2))
+    cost = 0
+    for c in text[1:]:
+        new = pos.copy()
+        key = np.array(np.where(layout == c)).reshape((2,))
+        if positioning == 'random':
+            index = np.random.randint(0, pointers)
+        new[index] = key
+        cost += np.sum(np.linalg.norm(pos - new, axis=0))
+        stretch_cost = np.linalg.norm(new[...,None] - new.T[None,...])
+        cost += stretch_cost * stretch_penalty
+        pos = new
+    return cost
