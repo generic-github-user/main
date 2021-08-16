37250bbe331fbd867404525cf682d934702693d5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-16 14:24:46 -0400

    Add querty keyboard layout as array of characters

--- keyboard.py
+++ keyboard.py
@@ -2,3 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import string
 import random
+
+
+layout = """
+1234567890
+qwertyuiop[]\\
+asdfghjkl;'
+zxcvbnm,./
+???? ?
+"""
+layout = list(filter(None, layout.splitlines()))
+max_len = len(max(layout, key=len))
+layout = [l+('?'*(max_len-len(l))) for l in layout]
+layout = list(map(list, layout))
+layout = np.array(layout, dtype=str)
+print(layout)
