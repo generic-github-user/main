ca1fa3c6787d6aae636e14ea4c92de65c0777129
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-29 22:45:05 -0400

    Generate word list from nltk corpus/frequency data

--- wordtetris.py
+++ wordtetris.py
@@ -10,3 +10,9 @@ import nltk
 from nltk import FreqDist
 from nltk.corpus import brown
 nltk.download('brown')
+
+word_limit = 20000
+word_frequencies = FreqDist(i.lower() for i in brown.words()).most_common()
+# print(word_frequencies.most_common()[:10])# words = word_frequencies.keys()[:10000]
+words = [a for a, b in word_frequencies[:word_limit]]
+words = [word.translate(str.maketrans('', '', string.punctuation)) for word in words]
