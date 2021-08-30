ab049a342d2c0da6140b301aedba98d03ecb450f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-29 22:46:34 -0400

    Calculate frequencies of individual symbols/characters from word list

--- wordtetris.py
+++ wordtetris.py
@@ -13,6 +13,17 @@ nltk.download('brown')
 
 word_limit = 20000
 word_frequencies = FreqDist(i.lower() for i in brown.words()).most_common()
-# print(word_frequencies.most_common()[:10])# words = word_frequencies.keys()[:10000]
+# print(word_frequencies.most_common()[:10])
+symbol_frequencies = {}
+# words = word_frequencies.keys()[:10000]
 words = [a for a, b in word_frequencies[:word_limit]]
 words = [word.translate(str.maketrans('', '', string.punctuation)) for word in words]
+for word, freq in word_frequencies[:word_limit]:
+    for char in word.lower():
+        if char in string.ascii_letters:
+            if char in symbol_frequencies:
+                symbol_frequencies[char] += freq
+            else:
+                symbol_frequencies[char] = freq
+print(symbol_frequencies)
+# breakpoint()
