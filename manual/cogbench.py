f3441aac099d4c37b55fcfa5dbf1aec66cef84cd
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-09-04 17:00:17 -0400

    Add Device class

--- manual/cogbench.py
+++ manual/cogbench.py
@@ -4,3 +4,10 @@ import string
 import random
 from fuzzywuzzy import fuzz, process
 import time
+class Device:
+    def __init__(self, name, device_type=None, screen_width=None, screen_height=None):
+        self.name = name
+        self.device_type = device_type
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.created = time.time()
