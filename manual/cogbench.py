2f6b1e27400d238ab746f44ee2d298f6b409c214
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-09-04 18:34:59 -0400

    Add function for handling command strings

--- manual/cogbench.py
+++ manual/cogbench.py
@@ -40,3 +40,20 @@ class Session:
 # )
 tests = [Alternation]
 test_names = [t.__name__.lower() for t in tests]
+
+# TODO: move functions to a class (?)
+def handle_command(sess=None, database=None):
+    command = input().split()
+    lead = command[0].lower()
+    save(sess=main_database)
+    matches = list(filter(lambda x: lead in x, test_names))
+    # if lead in test_names:
+    if matches:
+        test_name = matches[0]
+        test = tests[test_names.index(test_name)]()
+        if database:
+            database.tests.append(test)
+        else:
+            print('Warning: no database currently loaded; test will not be saved')
+        test.run()
+        save(sess=database)
