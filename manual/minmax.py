bc24a4b4c1c4654d7df83f15f484325ed4631838
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-09-06 17:48:48 -0400

    Add methods for min/max test

--- manual/minmax.py
+++ manual/minmax.py
@@ -12,3 +12,23 @@ class MinMax(Test):
         self.goal = goal
         self.values = np.random.randint(minimum, maximum, size=number)
         self.target = getattr(self.values, self.goal)()
+
+    def calculate_score(self):
+        return self.accuracy
+
+    def run(self):
+        self.started = time.time()
+        print(f'Find the {self.goal} of these values:')
+        for value in self.values:
+            print(f'[{value}]')
+        self.user_input = input()
+        self.ended = time.time()
+        self.elapsed = self.ended - self.started
+        self.accuracy = int(int(self.user_input) == self.target)
+        self.score = self.calculate_score()
+        return self
+
+    def upgrade(self, *args, **kwargs):
+        super().upgrade(*args, **kwargs)
+        if not hasattr(self, 'score'):
+            self.score = self.calculate_score()
