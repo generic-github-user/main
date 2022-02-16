14b0aa55e6c7168fea019496903c86d0cc98fc11
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-02-16 12:52:19 -0500

    Load database

--- eva.py
+++ eva.py
@@ -1,3 +1,10 @@
 import pickle
 import string
 import time
+
+databasePath = './eva-db';
+try:
+    with open(databasePath, 'rb') as fileRef:
+        nodes = pickle.load(fileRef);
+except:
+    nodes = [];
