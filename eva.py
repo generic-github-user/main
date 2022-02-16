77470c4d93f980e34a7179dc97a5e4f59731d414
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-02-16 12:54:17 -0500

    Get inputs and store in database

--- eva.py
+++ eva.py
@@ -8,3 +8,15 @@ try:
         nodes = pickle.load(fileRef);
 except:
     nodes = [];
+
+for i in range(1000):
+    newInput = input()
+    if newInput == 'quit':
+        quit()
+    elif newInput == 'print':
+        print(nodes)
+    else:
+        id = len(nodes)
+        nodes.append([id, newInput, [], time.time()])
+        with open(databasePath, 'wb') as fileRef:
+            pickle.dump(nodes, fileRef)
