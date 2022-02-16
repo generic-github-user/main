63ebd7a6f4e296bd5d898096d1879b27595a504d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-02-16 18:45:13 -0500

    Add basic relation extraction from inputs

--- eva.py
+++ eva.py
@@ -9,6 +9,7 @@ try:
 except:
     nodes = [];
 
+relations = ['are', 'is a', 'has', 'have'];
 for i in range(1000):
     newInput = input()
     if newInput == 'quit':
@@ -18,5 +19,17 @@ for i in range(1000):
     else:
         id = len(nodes)
         nodes.append([id, newInput, [], time.time()])
+        for r in relations:
+            r2 = f' {r} '
+            if r2 in newInput:
+                a, b = newInput.split(r2)
+                for ai in a.split(','):
+                    id_a = len(nodes)
+                    nodes.append([id_a, ai, [], time.time()])
+                    id_b = len(nodes)
+                    nodes.append([id_b, b, [], time.time()])
+                    id_c = len(nodes)
+                    nodes.append([id_c, r, [id_a, id_b], time.time()])
+                    nodes.append([len(nodes), 'source', [id_c, id], time.time()])
         with open(databasePath, 'wb') as fileRef:
             pickle.dump(nodes, fileRef)
