02666a8dc93b7c6a5ff8b81c320f1ea3748de67d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-01 00:02:54 -0500

    fix logging

--- eva.py
+++ eva.py
@@ -3,7 +3,12 @@ import string
 import time
 import json
 import random
+import sys
 from customize import ratings
+# from .. import giraffe
+
+sys.path.insert(0, '../giraffe')
+from giraffe import Graph
 
 databasePath = './eva-db'
 ignoredTypes = ['length', 'type', 'token', 'origin', 'label', 'group', 'rating']
@@ -111,15 +116,17 @@ def P(message):
 def updateAll():
     if debug:
         print('Updating database')
+
+    P('Consolidating identical nodes')
     for n in nodes:
         # print(f'Updating node {n[0]}: {n[1]}')
-        P('Consolidating identical nodes')
         duplicates = list(filter(lambda x: n[1]==x[1], nodes))
         exists = len(list(filter(lambda y: len(y[2])>0, duplicates))) > 0
         if len(duplicates) > 1 and not exists:
             addNode(n[1], [x[0] for x in duplicates])
 
-        P('Extract node data types')
+    P('Extracting node data types')
+    for n in nodes:
         if n[1] not in ignoredTypes:
             typeId = addNode(type(n[1]).__name__, [], False)
             # m = list(filter(lambda x: n[1]==x[1] and n[2]==x[2], nodes))
@@ -128,7 +135,8 @@ def updateAll():
             if (len(m) == 0):
                 addNode('type', [n[0], typeId])
 
-        P('Extract lengths from string nodes')
+    P('Extracting lengths from string nodes')
+    for n in nodes:
         if n[1] not in ignoredTypes and isinstance(n[1], str):
             lenId = addNode(len(n[1]), [], False)
             refSources = [nodes[z] for z in references[n[0]]]
@@ -139,8 +147,9 @@ def updateAll():
         #     for t in n[1].split():
                 # addNode()
 
-        P('Marking rating nodes')
-        # # TODO: move (some) input processing here
+    P('Marking rating nodes')
+    # # TODO: move (some) input processing here
+    for n in nodes:
         if n[1] not in ignoredTypes and isinstance(n[1], str) and len(n[1])==3 and n[1][1]=='.':
             for r in ratings:
                 if r[0]==n[1][0]:
