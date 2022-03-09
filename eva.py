38a96475b1e170b1a934aa1ca998261b443b777f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-09 18:21:28 -0500

    add simple mathematical expression parser

--- eva.py
+++ eva.py
@@ -138,6 +138,27 @@ database = Graph(nodes)
 # meta-inference
 
 
+def parseExpression(ex):
+    frame = []
+    valueType = None
+    value = None
+    for c in ex:
+        if c in string.digits:
+            if valueType == 'num':
+                value += c
+            else:
+                # last = frame
+                frame.append(value)
+                value = c
+                valueType = 'num'
+        elif (c in '+-*/^'):
+            if valueType == 'op':
+                value += c
+            else:
+                frame.append(value)
+                value = c
+                valueType = 'op'
+
 def getId():
     return len(nodes)
 
