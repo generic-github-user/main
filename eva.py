9e1bafbda3f872e9ffbe0fe29d64c305d7f0e287
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-09 17:45:27 -0500

    Object size computation function from https://stackoverflow.com/a/30316760

--- eva.py
+++ eva.py
@@ -13,6 +13,14 @@ import zlib
 import string
 # from .. import giraffe
 
+from types import ModuleType, FunctionType
+from gc import get_referents
+
+# Custom objects know their class.
+# Function objects seem to know way too much, including modules.
+# Exclude modules as well.
+BLACKLIST = type, ModuleType, FunctionType
+
 sys.path.insert(0, '../giraffe')
 # from giraffe import Graph
 
@@ -66,6 +74,25 @@ class Graph:
                 return matches[0].id
         self.updateNode(newId)
         return newId
+
+# https://stackoverflow.com/a/30316760
+def getsize(obj):
+    """sum size of object & members."""
+    if isinstance(obj, BLACKLIST):
+        raise TypeError('getsize() does not take argument of type: '+ str(type(obj)))
+    seen_ids = set()
+    size = 0
+    objects = [obj]
+    while objects:
+        need_referents = []
+        for obj in objects:
+            if not isinstance(obj, BLACKLIST) and id(obj) not in seen_ids:
+                seen_ids.add(id(obj))
+                size += sys.getsizeof(obj)
+                need_referents.append(obj)
+        objects = get_referents(*need_referents)
+    return size
+
 
 
 try:
