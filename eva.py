87a2168484e423a7659e784f0ef3e9ffd801f8f7
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-17 17:23:26 -0400

    Add nesting indents to improve readability

--- eva.py
+++ eva.py
@@ -333,16 +333,17 @@ def nodeProperty(node, attr, update=True):
 
 
 # related_to
-def say(content, source=None, intent='information', record=False):
+def say(content, source=None, intent='information', record=False, level=0):
     assert(isinstance(content, str))
     assert(isinstance(source, int) or source is None)
+    indents = ' ->*~'
 
     if record:
         newId = database.addNode('intent', [database.addNode(content, [], True), database.addNode(intent, [], False)])
         database.addNode('origin', [newId, database.addNode('eva_output', [], False)])
         if source is not None:
             database.addNode('source', [newId, source])
-    print(content)
+    print(('  ' * level)+indents[level]+' '+content)
     # return newId
 
 # logical_relations / relations
