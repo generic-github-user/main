bc81db51c03cd8068e33a58231e2e81914ce7a8e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-17 16:54:57 -0400

    Add HOF for tracking execution time of (some) function calls

--- eva.py
+++ eva.py
@@ -339,6 +339,24 @@ def say(content, source=None, intent='information', record=False):
 # expected_usefulness
 # "hanging" nodes (with intermediary inferences deleted)
 
+def timeFunc(F, level=0):
+    def timed(*args, **kwargs):
+        start = time.time()
+        say('Executing function', level=level+1)
+        fOut = F(*args, **kwargs)
+        end = time.time()
+        elapsed = end - start
+
+        src = [fOut] if (fOut is not None) else []
+        database.addNode('start_time', src + [database.addNode(start, [], False)], False, True, level=level+1)
+        database.addNode('end_time', src + [database.addNode(end, [], False)], False, True, level=level+1)
+        database.addNode('duration', src + [database.addNode(elapsed, [], False)], False, True, level=level+1)
+
+        say(f'Finished execution in {elapsed} seconds', level=level)
+
+        return fOut
+    return timed
+
 def think(node=None):
     start = time.time()
     if node is None:
