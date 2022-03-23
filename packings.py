c2bf715026f317bfca4f1c666598ec4de58aa7ee
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-23 15:47:10 -0400

    Add method for expanding polyomino representation to correct size

--- packings.py
+++ packings.py
@@ -12,3 +12,18 @@ class Polyomino:
         self.indices = [(0, 0)]
         self.neighbors = [0]
         self.size = 1
+
+    def pad(self):
+        # indices = np.transpose(self.data.nonzero())
+        padding = np.stack([
+            np.min(self.indices, axis=0)==0,
+            np.max(self.indices, axis=0)==(np.array(self.data.shape)-1)
+        ], axis=1).astype(int)
+        lower = padding[:, 0]
+        self.data = np.pad(
+            self.data,
+            padding
+        )
+        for i, ind in enumerate(self.indices):
+            self.indices[i] = tuple(np.array(ind)+lower)
+        return self
