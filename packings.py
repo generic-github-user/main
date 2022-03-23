5fac011eaea53ee64e3000b075845bdac12e2f84
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-23 15:45:28 -0400

    Add Polyomino class

--- packings.py
+++ packings.py
@@ -5,3 +5,10 @@ class Collection:
         if members is None:
             members = []
         self.members = members
+
+class Polyomino:
+    def __init__(self, size=100):
+        self.data = np.ones((1, 1))
+        self.indices = [(0, 0)]
+        self.neighbors = [0]
+        self.size = 1
