c6c3b2de5fd520adcbe0948bc066153f9fd81f9e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-03-23 15:41:06 -0400

    Add class for representing collections of polyominoes

--- packings.py
+++ packings.py
@@ -1,2 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+class Collection:
+    def __init__(self, members=None):
+        if members is None:
+            members = []
+        self.members = members
