a68c5a9376dfb1b9e667fe0800cd119041b0bafd
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-04-22 14:55:51 -0400

    Move loadBackup command

--- eva.py
+++ eva.py
@@ -437,24 +437,6 @@ def findCommand(newInput):
         database.addNode('origin', [n.id, database.addNode('eva_output', [], False)])
     return searchNode
 
-@command('loadbackup')
-def loadbackupCommand(newInput):
-    with open('eva_03_17_2022, 11_41_04.evab', 'rb') as fileRef:
-        database.nodes = pickle.loads(zlib.decompress(fileRef.read()))
-        database.nodes = list(map(lambda n: database.nodeTemplate(*n), database.nodes))
-
-        database.references = []
-        print(f'Building reference lists for {len(database.nodes)} nodes')
-        for i, n in enumerate(database.nodes):
-            if (i % 1000 == 0):
-                say(f'Processed {i} nodes')
-            if n.value not in ['origin', 'accessed', 'source', 'type']:
-                database.references.append([m.id for m in database.nodes if (n.id in m.members)])
-            else:
-                database.references.append([])
-        print('Done')
-    database.save()
-
 ppc = pyparsing.pyparsing_common
 pyparsing.ParserElement.enablePackrat()
 # sys.setrecursionlimit(3000)
