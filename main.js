e27013daed540dc4e5175bd00716e978ca552400
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-04-26 23:13:31 -0400

    add ndarray class

---
+++ main.js
@@ -0,0 +1,10 @@
+class ndarray {
+      constructor(dimensions) {
+            this.dimensions = dimensions;
+            this.data = new Array(dimensions[0]).fill(0);
+            this.update();
+            if (dimensions.length > 1) {
+                  this.data = this.data.map(x => new ndarray(dimensions.slice(1)));
+            }
+      }
+}
