0e820007efcd97afc3d1effec62f12d418a47e98
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-07 11:39:08 -0400

    Add ANSI escape codes for printing colored text

--- packings.c
+++ packings.c
@@ -6,6 +6,16 @@
 int MAX_WIDTH = 10;
 int MAX_BLOCKS = 100;
 
+// Based on code from https://stackoverflow.com/a/3219471
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
+// char COLOR_ORDER = { RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA };
 
 struct vector {
 	int x, y, z;
