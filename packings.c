7e4c2a5e6d10139d84c978d0abb0a2a264974ea3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-07 03:53:02 -0400

    add vector and array structs

--- packings.c
+++ packings.c
@@ -2,3 +2,19 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
+
+int MAX_WIDTH = 10;
+int MAX_BLOCKS = 100;
+
+
+struct vector {
+	int x, y, z;
+};
+
+struct array {
+	int rank;
+	int* shape;
+	int size;
+	int* data;
+};
+
