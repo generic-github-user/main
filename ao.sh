29a9c6169117833dbf73cf858216bf222a322764
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-08 18:57:00 -0400

    set globbing options with shopt

---
+++ ao.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# A simple script for organizing my desktop and other folders that tend to get cluttered
+shopt -s nocaseglob
+shopt -s globstar
+#shopt -s nullglob
+shopt -s extglob
+shopt -s dotglob
