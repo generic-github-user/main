120e90dc9996d59896ecf69a5152132648b19be6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-08 19:06:39 -0400

    add additional metadata and path settings

--- ao.sh
+++ ao.sh
@@ -6,3 +6,18 @@ shopt -s globstar
 #shopt -s nullglob
 shopt -s extglob
 shopt -s dotglob
+
+restrict='@(img_archive)'
+
+#ls -al > ao_ls.txt
+ls -al >> ./aolog
+indexname=./imgindex.csv
+
+imgtypes={png,jpg,jpeg,webp,gif}
+imt='@(png|jpg|jpeg|webp|gif)'
+vidtypes='@(mov|mp4)'
+
+S="::"
+#sources="@(~/Desktop|~/Downloads|~/Desktop/January)"
+sources='@(.|../Downloads|January)'
+echo $sources
