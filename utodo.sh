5a7538fb79f8e0ca5ecd8e6dfd191d1e18d79d89
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-25 15:22:07 -0400

    back up tasks whenever update script is run

--- utodo.sh
+++ utodo.sh
@@ -8,3 +8,7 @@ grep -va -- "--" todo.txt | sponge todo.txt
 # Clone recurring tasks to main todo list
 IFS=$'\n'
 grep -e "-f d" -e "-daily" recurring.txt | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf todo.txt -f "complete.txt" >> todo.txt
+# Make a quick and dirty backup of the todo list
+b="todo_backups/todo_$(date +%s).tar.gz"
+tar czf $b todo.txt
+echo "Backed up $(wc -c < $b) bytes to $b"
