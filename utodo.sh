172ac517c9f280354b55c7e33dfe7d91d9c6810c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-25 15:15:36 -0400

    generate specified instances of recurring tasks

--- utodo.sh
+++ utodo.sh
@@ -5,3 +5,6 @@ cd ~/Desktop
 # Move completed tasks to archive
 grep -- "--" todo.txt | sed -e 's/--//g' | tee >(cat - >> "complete.txt") >(echo "$(wc -l) completed tasks moved")
 grep -va -- "--" todo.txt | sponge todo.txt
+# Clone recurring tasks to main todo list
+IFS=$'\n'
+grep -e "-f d" -e "-daily" recurring.txt | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf todo.txt -f "complete.txt" >> todo.txt
