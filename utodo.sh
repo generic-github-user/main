c487c5a245d9e2f01fa7afa8d1c75627a8903003
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-26 15:34:06 -0400

    improve flag handling and recurring task management

--- utodo.sh
+++ utodo.sh
@@ -4,11 +4,14 @@
 main=todo.txt
 cd ~/Desktop
 # Move completed tasks to archive
-grep -- "--" $main | sed -e 's/--//g' | tee >(cat - >> "complete.txt") >(echo "$(wc -l) completed tasks moved")
-grep -va -- "--" $main | sponge $main
+grep -e '--' -e ' -archive' -e ' -cc' -- $main | sed -e 's/--//g' | tee >(cat - >> "complete.txt") >(echo "$(wc -l) completed tasks moved")
+grep -va -e '--' -e ' -archive' -e ' -cc' -- $main | sponge $main
 # Clone recurring tasks to main todo list
 IFS=$'\n'
-grep -e "-f d" -e "-daily" recurring.txt | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf $main -f "complete.txt" >> $main
+filter='s/ -archive| -cc| -daily| -[fd] \S+//'
+sed -E -e $filter complete.txt > complete.temp.txt
+sed -E -e $filter todo.txt > todo.temp.txt
+grep -e "-f d" -e "-daily" recurring.txt | sed -E -e $filter | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf "todo.temp.txt" -f "complete.temp.txt" >> $main
 # Make a quick and dirty backup of the todo list(s)
 if [[ $1 == '-b' ]]; then
 	targets="todo complete recurring"
