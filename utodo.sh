9b78e1b1072ec9d0bc1be66842cca81aae50e58f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-26 13:39:03 -0400

    mild refactoring

--- utodo.sh
+++ utodo.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Update todo list
 
+main=todo.txt
 cd ~/Desktop
 # Move completed tasks to archive
-grep -- "--" todo.txt | sed -e 's/--//g' | tee >(cat - >> "complete.txt") >(echo "$(wc -l) completed tasks moved")
-grep -va -- "--" todo.txt | sponge todo.txt
+grep -- "--" $main | sed -e 's/--//g' | tee >(cat - >> "complete.txt") >(echo "$(wc -l) completed tasks moved")
+grep -va -- "--" $main | sponge $main
 # Clone recurring tasks to main todo list
 IFS=$'\n'
-grep -e "-f d" -e "-daily" recurring.txt | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf todo.txt -f "complete.txt" >> todo.txt
+grep -e "-f d" -e "-daily" recurring.txt | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf $main -f "complete.txt" >> $main
 # Make a quick and dirty backup of the todo list
 b="todo_backups/todo_$(date +%s).tar.gz"
-tar czf $b todo.txt
+tar czf $b $main
 echo "Backed up $(wc -c < $b) bytes to $b"
