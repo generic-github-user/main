c5847107a6fe2917fbf6c5fc620e3b97e56c0d18
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-27 00:32:26 -0400

    update script to use .todo file extension

--- utodo.sh
+++ utodo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Update todo list
 
-main=todo.txt
+set -e
+main=.todo
 cd ~/Desktop
 
 # Move completed tasks to archive
@@ -11,19 +12,19 @@ grep -va -e '--' -e ' -archive' -e ' -cc' -- $main | sponge $main
 # Clone recurring tasks to main todo list
 IFS=$'\n'
 filter='s/ -archive| -cc| -daily| -[fd] \S+| #\w+//g;s/\s*$//'
-sed -E -e "$filter" complete.txt > complete.temp.txt
-sed -E -e "$filter" todo.txt > todo.temp.txt
-grep -e "-f d" -e "-daily" recurring.txt | sed -E -e "$filter" | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf "todo.temp.txt" -f "complete.temp.txt" | tee -a $main
-rm -v todo.temp.txt complete.temp.txt
+sed -E -e "$filter" complete.todo > complete.temp.todo
+sed -E -e "$filter" $main > todo.temp.todo
+grep -e "-f d" -e "-daily" recurring.todo | sed -E -e "$filter" | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf "todo.temp.todo" -f "complete.temp.todo" | tee -a $main
+rm -v todo.temp.todo complete.temp.todo
 
 # Make a quick and dirty backup of the todo list(s)
 if [[ $1 == '-b' ]]; then
-	targets="todo complete recurring"
+	targets="$main complete.todo recurring.todo"
 	IFS=$' '
 	#for t in "${targets[@]}"; do
 	for t in $targets; do
 		b="todo_backups/${t}_$(date +%s).tar.gz"
-		tar czf "$b" "$t.txt"
+		tar vczf "$b" "$t"
 		echo "Backed up $(wc -c < $b) bytes to $b"
 	done
 fi
