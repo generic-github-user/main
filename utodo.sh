c7324f670ff5903c4a747a7a6a4f07b19f706730
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-07-09 19:35:00 -0400

    various improvements to todo processing

--- utodo.sh
+++ utodo.sh
@@ -6,18 +6,18 @@ source ~/Desktop/ao/ao_config.sh
 
 # Move completed tasks to archive
 cd $todopath
-grep -e '--' -e ' -archive' -e ' -cc' -- $tmain | sed -e 's/--//g' | tee >(cat - >> complete.todo) >(echo "$(wc -l) completed tasks moved")
-grep -va -e '--' -e ' -archive' -e ' -cc' -- $tmain | sort -n | sponge $tmain
+grep -f cflags -- $tmain | sed -e 's/--//g' | tee >(cat - >> $todopath/complete.todo) >(echo "$(wc -l) completed tasks moved")
+grep -va -f cflags -- $tmain | sort -n | sponge $tmain
 echo -e ""
 
 # Clone recurring tasks to main todo list
 echo "Importing recurring tasks"
 IFS=$'\n'
-filter='s/ -archive| -cc| -daily| -[fd] \S+| #\w+//g;s/\s*$//'
+filter='s/ -cc//g;s/\s*$//'
 sed -E -e "$filter" complete.todo > complete.temp.todo
 sed -E -e "$filter" $tmain > todo.temp.todo
-grep -e "-f d" -e "-daily" $main/recurring.todo | sed -E -e "$filter" | sed -e "s/$/ $(date +'%a, %b %d')/" | grep -xvf todo.temp.todo -f complete.temp.todo | tee -a $tmain
-rm -v todo.temp.todo complete.temp.todo
+grep -e "-f d" -e "-daily" $main/recurring.todo | sed -e "s/$/ -t $(date +'%m-%d')/" | grep -Fxv -f todo.temp.todo -f complete.temp.todo | tee -a $tmain
+#rm -v todo.temp.todo complete.temp.todo
 echo -e ""
 
 #echo $(pwd)
@@ -32,3 +32,5 @@ if [[ $1 == '-b' ]]; then
 	echo "Backed up $(wc -c < $b) bytes to $b"
 	#done
 fi
+
+sort -n $tmain | sponge $tmain
