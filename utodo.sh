e642e34a0be29eac44e269a376faef0a4168d5cc
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-07-09 20:47:45 -0400

    add some comments to utodo.sh

--- utodo.sh
+++ utodo.sh
@@ -13,12 +13,17 @@ echo -e ""
 # Clone recurring tasks to main todo list
 echo "Importing recurring tasks"
 IFS=$'\n'
+# Remove any lingering completion flags; strip trailing whitespace
 filter='s/ -cc//g;s/\s*$//'
 sed -E -e "$filter" complete.todo > complete.temp.todo
 sed -E -e "$filter" $tmain > todo.temp.todo
 inst () {
+	# find recurring tasks -> append dates -> filter out tasks that have been
+	# completed or are already in the todo list
 	grep -e $1 -e $2 $main/recurring.todo | sed -e "s/$/ -t $(date +$3)/" | grep -Fxv -f todo.temp.todo -f complete.temp.todo | tee -a $tmain
 }
+# I use - to denote incrementing the granularity (year, month, week, etc.) of
+# the date component by one and ~ for two, but this can of course be customized
 inst "-f d" "-daily" '%m-%d'
 inst "-f w" "-weekly" '%Y~%W'
 inst "-f m" "-monthly" '%Y-%m'
@@ -39,3 +44,4 @@ if [[ $1 == '-b' ]]; then
 fi
 
 sort -n $tmain | sponge $tmain
+cp $tmain $todopath/snapshot.todo
