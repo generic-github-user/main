ef6999938e1ef3663b828495f8bc785d2805d7fa
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-07-20 18:58:20 -0400

    add ap.py

---
+++ ap/ap.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import shutil
+
+import pickle
+import time
+import re
+import itertools
+
+import warnings
+import textwrap
+
+import psutil
+
+dbpath = '/home/alex/Desktop/ap.pickle'
+timelimit = 20
+
+loglevel = 0
