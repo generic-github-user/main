931b100a6a13e57d3fcbef4c76d9a2f59339046a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-07-21 11:18:42 -0400

    add more helper functions (log and save)

--- ap/ap.py
+++ ap/ap.py
@@ -11,6 +11,7 @@ import warnings
 import textwrap
 
 import psutil
+import hashlib
 
 dbpath = '/home/alex/Desktop/ap.pickle'
 timelimit = 20
@@ -57,3 +58,33 @@ def hashfile(path):
             md5.update(data)
             sha1.update(data)
     return (md5.hexdigest(), sha1.hexdigest())
+
+#try:
+#    with open(dbpath, 'rb') as f:
+#        data = pickle.load(f)
+#except Exception as E:
+#    print(E)
+#    data = {
+#        'snapshots': [],
+#        'files': []
+#    }
+
+if len(sys.argv) > 1 and sys.argv[1] == 'CLEAR':
+    data = {
+        'snapshots': [],
+        'files': []
+    }
+
+def save(path=dbpath):
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
+
+def log(content, level=0):
+    n=60
+    lines = [content[i:i+n] for i in range(0, len(content), n)]
+    #print('  '*level+content)
+    #print(lines[0])
+    #print('\n~ '.join(lines))
+
+    p='  ' * level # prefix
+    print(p + f'\n{p}~ '.join(textwrap.wrap(content, n)))
