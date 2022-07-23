b6007449d2d9cfee170de207e8f5831fcf816cc8
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-07-23 14:45:56 -0400

    fix some bugs, misc todo class improvements

--- ap/todo.py
+++ ap/todo.py
@@ -2,17 +2,22 @@ import pickle
 import datetime
 import re
 import time
+import os
+import dateparser
 
-dbpath = '~/Desktop/todo.pickle'
-todopath = '~/Desktop/.todo'
+dbpath = os.path.expanduser('~/Desktop/todo.pickle')
+todopath = os.path.expanduser('~/Desktop/.todo')
 
 try:
     with open(dbpath, 'rb') as f:
         data = pickle.load(f)
+except FileNotFoundError:
+    data = []
 
 
 class todo:
-    def __init__(self, content) -> todo:
+    def __init__(self, raw, content=''):
+        self.raw = raw
         self.content = content
         self.done = False
         self.donetime = None
@@ -20,6 +25,12 @@ class todo:
         self.tags = []
         self.created = time.time()
         self.importance = 0
+        self.line = None
         self.location = ''
         self.sub = []
         self.parent = None
+
+    def __str__(self):
+        inner = [self.content, f'<{self.tags}>']
+        inner = "\n\t".join(inner)
+        return f'todo {{ {inner} }}'
