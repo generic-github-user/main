82e6d45f812ecf780cf8aadd2cd6ab832cb1c392
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-07-29 20:49:27 -0400

    add class for representing tokens in AST (parsed by Lark)

--- transpiler/parser.py
+++ transpiler/parser.py
@@ -13,6 +13,18 @@ class TreeIndenter(Indenter):
 with open('grammar.lark', 'r') as grammar:
     parser = Lark(grammar.read(), parser='lalr', lexer='contextual', postlex=TreeIndenter(), debug=True)
 
+class Token:
+    def __init__(self, source, parent=None):
+        self.parent = parent
+        self.source = source
+        for attr in 'type value line column'.split():
+            setattr(self, attr, getattr(self.source, attr))
+            self.length = len(self.source)
+
+    def text(self): return self.value
+
+    def __str__(self):
+        return f'Token <{self.type}, {self.line}:{self.column}> {self.value}'
 with open('example.fn', 'r') as f:
     parsed = parser.parse(f.read())
 print(parsed.pretty()[:2000])
