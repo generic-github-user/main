8088e2578f87e8c34517688f1f78015032acf1cc
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-24 12:26:27 -0400

    add union type that represents any one of two or more distinct types

--- pythings/pythings.py
+++ pythings/pythings.py
@@ -8,3 +8,19 @@ class Type:
     def __init__(self, name=None, **kwargs):
         if name is not None:
             self.name = name
+
+    def __str__(self):
+        if hasattr(self, 'name'): return self.name
+        else: return type(self).__name__
+
+    __repr__ = __str__
+
+    def __or__(self, b):
+        return UnionType(self, b)
+
+class UnionType(Type):
+    def __init__(self, *members):
+        self.members = members
+
+    def validate(self, x):
+        return any(m.validate(x) for m in self.members)
