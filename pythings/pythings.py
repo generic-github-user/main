bac0cf8e312e7b1cba3a242948ec6913082b0426
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-26 16:03:03 -0400

    improve documentation for Rect example class

--- pythings/pythings.py
+++ pythings/pythings.py
@@ -487,14 +487,23 @@ class Rect:
     A simple rectangle class, implemented using the `Point` class.
     """
 
-    pos: Point = Attr('Location of the lower-left corner of the rectangle (assume 2D Euclidean space)')
-    delta: Point = Attr('Positive (Q1) point representing the difference between opposite corners of the rectangle (including `pos`)')
+    pos: Point = Attr("""Location of the lower-left corner of the rectangle
+                      (assume 2D Euclidean space)""")
+    delta: Point = Attr("""Positive (Q1) point representing the difference
+    between opposite corners of the rectangle (including `pos`)""")
 
-    def area(self) -> Number: return self.delta.x * self.delta.y
-    area.__doc__ = """Returns the rectangle's area"""
+    def area(self) -> Number:
+        """Returns the rectangle's area"""
+        return self.delta.x * self.delta.y
 
-    def perimeter(self) -> Number: return (2 * self.delta.x) + (2 * self.delta.y)
-    def scale(self, x) -> Number: return Rect(pos, delta * x)
+    def perimeter(self) -> Number:
+        """Returns the perimeter ("edge length") of the rectangle"""
+        return (2 * self.delta.x) + (2 * self.delta.y)
+
+    def scale(self, x: Number >= 0):
+        """Scales this rectangle by the given factor (`x`) and returns a new
+        `Rect`"""
+        return Rect(pos, delta * x)
     
 print(Rect.doc('markdown'))
 print(Rect.doc('text'))
