6d10835d1bcc30c90a7a137b49740784cc07f62d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-30 22:26:53 -0400

    filter out tasks marked as complete and append to an archive file

--- tools/ytodo.py
+++ tools/ytodo.py
@@ -13,3 +13,17 @@ pathlib.Path('./todo-backup').mkdir(exist_ok=True)
 timestamp = datetime.datetime.now().strftime('%d-%m-%s_%H-%M-%S')
 with open(f'./todo-backup/todo-{timestamp}.yaml', 'w') as bfile:
     bfile.write(source)
+
+done = []
+notdone = []
+for task in todos:
+    if isinstance(task, str):
+        info = task
+    elif isinstance(task, dict):
+        info = list(task)[0]
+    if '--' in info: done.append(task)
+    else: notdone.append(task)
+with open('./ydone.yaml', 'a') as dfile:
+    dfile.write(yaml.dump(done))
+with open(path, 'w') as sfile:
+    sfile.write(yaml.dump(notdone))
