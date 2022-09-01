23197bb19b4d95408f347eccb1158b14677dce75
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-01 16:19:17 -0400

    add slightly more complex sample (borrowed from python-snippets)

---
+++ sample-2.py
@@ -0,0 +1,10 @@
+def plural(a, b, prepend=False):
+    if b != 1:
+        if a[-1] == 'y':
+            a = a[:-1] + 'ies'
+        elif a[-1] != 's':
+            a += 's'
+    if prepend:
+        a = f'{b} {a}'
+    return a
+
