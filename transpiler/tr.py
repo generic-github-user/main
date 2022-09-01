ec7d79305503e4ba3b4c76a83713b895c2ee74d0
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-01 16:35:17 -0400

    transpile top-level AST nodes

--- transpiler/tr.py
+++ transpiler/tr.py
@@ -7,6 +7,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument('source')
 
 
+def transpile(source, target: str) -> str:
+    match target:
+        case 'js':
+            match type(source):
+                case ast.Module:
+                    return '\n'.join(transpile(s, target) for s in source.body)
+                case _ if isinstance(source, list):
+                    return '\n'.join(transpile(s, target) for s in source)
+
+                case _: raise NotImplementedError(source, type(source))
+        case _: raise NotImplementedError
+
+
 args = parser.parse_args()
 content = pathlib.Path(args.source).read_text()
 tree = ast.parse(content)
