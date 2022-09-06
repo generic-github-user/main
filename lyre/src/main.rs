e8771aa100ae264969f419651991c38749175c25
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-06 19:02:54 -0400

    add ValueType enum

--- lyre/src/main.rs
+++ lyre/src/main.rs
@@ -130,6 +130,23 @@ impl<'a> Node<'a> {
 struct Value<'a> {
     vtype: String,
     value: &'a Node<'a>
+
+#[derive(Debug)]
+enum ValueType {
+    i8(i8),
+    i16(i16),
+    i32(i32),
+    i64(i64),
+    i128(i128),
+    u8(u8),
+    u16(u16),
+    u32(u32),
+    u64(u64),
+    u128(u128),
+    f32(f32),
+    f64(f64),
+    bool(bool),
+    char(char)
 }
 
 // fn main () -> Result<Value, Error> {
