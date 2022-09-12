4d823c68cf69539fc32260c7f4f66b0e7867817d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-12 03:50:15 -0400

    render ASCII representation of points in simulation

--- fluid-sim/src/main.rs
+++ fluid-sim/src/main.rs
@@ -24,4 +24,23 @@ fn main() {
             rng.gen_range(0.0..10.0),
         ));
     }
+
+    let res = 20;
+
+    for _i in 0..100 {
+        let mut grid = Array2::<bool>::from_elem((res, res), false);
+        for p in points.iter() {
+            grid[[
+                (p.x.round() as usize).clamp(0, res-1),
+                (p.y.round() as usize).clamp(0, res-1)]]= true;
+        }
+        // for ((x, y), value) in grid.indexed_iter() {
+        for row in grid.axis_iter(Axis(0)) {
+            println!("{}", row.to_vec().iter()
+                     .map(|v| if *v {".".to_string()} else {" ".to_string()})
+                     .collect::<Vec<String>>().join(""));
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    }
 }
