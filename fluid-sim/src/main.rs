c3c5c50f72202c40e77c662741565fbfa20aa1ea
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-12 03:46:34 -0400

    add point struct (and constructor function)

--- fluid-sim/src/main.rs
+++ fluid-sim/src/main.rs
@@ -2,3 +2,15 @@ use derive_more::{Add, Div};
 use rand::Rng;
 use std::{thread, time::Duration};
 use ndarray::{Array2, Axis};
+
+#[derive(Add, Div)]
+struct Point {
+    x: f64,
+    y: f64
+}
+
+impl Point {
+    fn new(x: f64, y: f64) -> Point {
+        return Point { x, y };
+    }
+}
