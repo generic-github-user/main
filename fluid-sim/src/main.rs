1437f4aaaad6f7034f8501858d053c8a2b5b1700
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-12 03:47:55 -0400

    randomly generate set of points to initialize simulation

--- fluid-sim/src/main.rs
+++ fluid-sim/src/main.rs
@@ -14,3 +14,14 @@ impl Point {
         return Point { x, y };
     }
 }
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let mut points = vec![];
+    for _i in 0..100 {
+        points.push(Point::new(
+            rng.gen_range(0.0..10.0),
+            rng.gen_range(0.0..10.0),
+        ));
+    }
+}
