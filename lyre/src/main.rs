5db2fbe0b2de6c6f6d795b76622951af5f207b02
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-11 21:40:02 -0400

    move Node struct and NodeType enum to node module

--- lyre/src/main.rs
+++ lyre/src/main.rs
@@ -14,67 +14,9 @@ use token::Token;
 mod chartype;
 use chartype::CharType;
 
-/// An AST node (more accurately, a [sub]tree); generally, these nodes will either be a sequence of
-/// tokens or other nodes, but not both
-#[derive(Debug, Clone)]
-struct Node {
-    /// The symbol corresponding to this node, if it is a leaf node
-    content: Option<Token>,
+mod node;
+use node::{Node, NodeType};
 
-    /// Sub-nodes forming the tree rooted at this node
-    // children: Vec<&'a Node<'a>>,
-    children: Vec<Node>,
-
-    nodetype: NodeType
-}
-
-#[derive(PartialEq, Debug, Clone)]
-enum NodeType {
-    Form,
-    Token,
-    Program,
-    String,
-    Integer,
-    Whitespace,
-    None
-}
-
-/// General Node struct methods
-impl<'a> Node {
-    /// Takes a list of indices and returns the final node in the path they describe (in which each
-    /// index corresponds to a child node/subnode in the next level of the tree)
-    fn get(&'a mut self, path: Vec<usize>) -> &'a mut Node {
-        let mut result = self;
-        for i in path.clone() {
-            if i >= result.children.len() {
-                eprintln!("{:?}", path.clone());
-                // self.print(0);
-            }
-            result = &mut result.children[i];
-        }
-        return result;
-    }
-
-    fn print(&self, level: u8) -> () {
-        print!("{}", "  ".repeat(level as usize));
-        let ntype = self.nodetype.clone();
-        print!("<{:?}> {}\n", ntype, self);
-        for c in self.children.iter() {
-            c.print(level+1);
-        }
-    }
-}
-
-/// Enables the Display and ToString traits for Node structs
-impl<'a> fmt::Display for Node {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}",
-            self.content.iter()
-            .map(|t| t.to_string())
-            .collect::<Vec<String>>()
-            .join(" "))
-    }
-}
 
 macro_rules! op_match {
     ($op:tt, $r:ident, $v:ident) => {
