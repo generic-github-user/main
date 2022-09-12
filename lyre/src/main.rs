e359b7fde32fe7b3907071c866b2bbcf25541b0c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-11 21:49:02 -0400

    move lexing function to separate module

--- lyre/src/main.rs
+++ lyre/src/main.rs
@@ -18,81 +18,8 @@ use node::{Node, NodeType};
 mod value;
 use value::Value;
 
-
-fn lex(buffer: BufReader<File>) -> Result<Vec<Token>, Error> {
-    // This section has a simple lexer that splits lyre source code into tokens, groups of
-    // consecutive characters with similar syntactic purposes
-
-    // A list of tokens parsed from the input program's source code
-    let mut tokens = Vec::<Token>::new();
-    for line in buffer.lines() {
-        println!("Parsing line: {}", line.as_ref().unwrap());
-        // Stores the type of the previous character
-        let mut ptype = CharType::None;
-        // Stores the type of the current character
-        let mut ctype = CharType::None;
-        // The text content of the current token; incrementally grown as new characters are
-        // consumed
-        let mut current: String = String::from("");
-
-        // Iterate over characters in line
-        for c in line?.chars() {
-            // Categorize characters by syntactic type (refer to the CharType enum for information
-            // about specific types)
-            // if c.is_alphanumeric() { ctype = CharType::Alphanumeric; }
-            if c.is_digit(10) { ctype = CharType::Digit; }
-            else if c.is_alphabetic() { ctype = CharType::Letter; }
-            else if c == '\n' { ctype = CharType::Newline; }
-            else if c.is_whitespace() { ctype = CharType::Whitespace; }
-            else if c == '[' { ctype = CharType::LeftSB; }
-            else if c == ']' { ctype = CharType::RightSB; }
-            else if c == '"' && ptype != CharType::String { ctype = CharType::String; }
-            else { ctype = CharType::Symbol; }
-
-            if ptype == CharType::String && c != '"' {
-                ctype = CharType::String;
-            }
-            println!("Lexing character {} as {:?}", c, ctype);
-
-            // If the character type is unchanged from the previous step, append it to the current
-            // token string ...
-            if ctype == ptype && ctype != CharType::LeftSB && ctype != CharType::RightSB {
-                current += String::from(c).as_ref();
-            }
-            // ... otherwise, start a new token and store the current one
-            else {
-                if ptype != CharType::None {
-                    let ntoken = Token {
-                        content: if ptype == CharType::String { String::from(&current[1..]) }
-                                 else { current },
-                        chartype: ptype
-                    };
-                    println!("Finished token {:?}", ntoken);
-                    tokens.push(ntoken);
-                }
-                current = String::from(c);
-            }
-
-            ptype = ctype;
-        }
-
-        let ntoken = Token {
-            content: current,
-            chartype: ptype
-        };
-        println!("Finished token {:?}", ntoken);
-        tokens.push(ntoken);
-
-        // Insert newline character after parsing line
-        tokens.push(Token { 
-            content: String::from(""),
-            chartype: CharType::Newline
-        });
-    }
-    println!("Parsed {} tokens", tokens.len());
-    // println!("{}", tokens);
-    Ok(tokens)
-}
+mod lexer;
+use lexer::lex;
 
 fn main () -> Result<(), Error> {
     let path = "sample.ly";
