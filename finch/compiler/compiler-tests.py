304019456a5c850ac25d109b9ce5f682ba2d5a35
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-19 17:19:42 -0400

    misc. test fixes

--- finch/compiler/compiler-tests.py
+++ finch/compiler/compiler-tests.py
@@ -2,19 +2,21 @@ import unittest
 # from cinch import parse, Token, Int, Float
 from ftoken import Token
 from node import Int, Float
+from chartype import CharType
+from lex import lex
 from parse import parse
 
 
 # Based on example(s) from https://docs.python.org/3/library/unittest.html
 class TestParser(unittest.TestCase):
     def test_int_literal(self):
-        self.assertEqual(parse("42"), Int([Token("42")]))
+        self.assertEqual(parse(lex("42")), Int([Token("42", CharType.Digit)]))
 
     def test_float_literal(self):
-        self.assertEqual(parse("4.2"), Float([Token("4.2")]))
+        self.assertEqual(parse(lex("4.2")), Float([Token("4.2")]))
 
     def test_string_literal(self):
-        self.assertEqual(parse('"forty-two"'), Float([Token('"forty-two"')]))
+        self.assertEqual(parse(lex('"forty-two"')), Float([Token('"forty-two"', CharType.String)]))
 
 
 if __name__ == "__main__":
