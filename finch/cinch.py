9d13868bce10965d4106fd8e2a724d377827050d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-18 23:57:48 -0400

    adapt Token and Node classes for custom parser
    i.e., no lark classes to translate the AST from

--- finch/cinch.py
+++ finch/cinch.py
@@ -9,42 +9,33 @@ class ArgumentError(Exception):
 
 
 class Token:
-    def __init__(self, source, parent=None):
+    def __init__(self, content, parent=None):
         self.parent = parent
-        self.source = source
-        for attr in 'type value line column'.split():
-            setattr(self, attr, getattr(self.source, attr))
-            self.length = len(self.source)
+        self.content = content
 
-    def text(self): return self.value
+    def text(self): return self.content
 
     def __str__(self):
-        return f'Token <{self.type}, {self.line}:{self.column}> {self.value}'
+        return f'Token <{self.type}, {self.line}:{self.column}> {self.content}'
 
 
 class Node:
-    def __init__(self, source, parent=None, depth=0, root=None):
-        self.parent = parent
-        self.root = root if root else self
-        self.children = []
-        self.source = source
+    def __init__(self, nodetype, parent=None, depth=0, root=None):
+        self.type = nodetype
 
-        self.meta = self.source.meta
-        self.type = self.source.data
+        self.root = root if root else self
+        self.parent = parent
+        self.children = []
+
         self.depth = depth
-        for c in self.source.children:
-            if isinstance(c, lark.Tree): self.children.append(
-                    Node(c, self, self.depth+1, root=self.root if self.root else self))
-            elif c is None: self.children.append(c)
-            else:
-                assert isinstance(c, lark.Token), c
-                self.children.append(Token(c))
 
     def text(self):
         return ''.join(c.text() for c in self.children)
 
     def __str__(self):
-        return f'Node <{self.type}> ({self.depth})' + '\n' + '\n'.join('  '*self.depth + str(n) for n in self.children)
+        return f'Node <{self.type}> ({self.depth})' +\
+               '\n' +\
+               '\n'.join('  '*self.depth + str(n) for n in self.children)
 
 
 if len(sys.argv) < 2:
