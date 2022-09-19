cd77e8313ba951f4681004552a42f032e43102c3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-19 09:31:47 -0400

    move parsing code into a function

--- finch/cinch.py
+++ finch/cinch.py
@@ -196,89 +196,94 @@ class Comment(Node):
         print("  "*depth + "[Comment elided]")
 
 
-tree = Block()
-stack = [tree]
-current = tree
-depth = 0
-for token in tokens:
-    current = stack[-1]
-    depth = len(stack)
-    if isinstance(current, String) and token.type != CharType.Quote:
-        current.add(token)
-        continue
-    match token.type:
-        case CharType.Quote:
-            if isinstance(current, String):
-                stack.pop()
-            else:
-                nnode = String()
-                current.add(nnode)
-                stack.append(nnode)
-
-        case CharType.Punctuation:
-            match token.content:
-                case "(":
-                    nnode = Tuple()
-                    if isinstance(current[-1], Expression):
-                        nnode_inner = Call(
-                            [current[-1], nnode],
-                            depth=depth
-                        )
-                        current[-1] = nnode_inner
-                        stack.append(nnode_inner)
-
+def parse(tokens: list[Token]) -> Node:
+    tree = Block()
+    stack = [tree]
+    current = tree
+    depth = 0
+    for token in tokens:
+        current = stack[-1]
+        depth = len(stack)
+        if isinstance(current, String) and token.type != CharType.Quote:
+            current.add(token)
+            continue
+        match token.type:
+            case CharType.Quote:
+                if isinstance(current, String):
+                    stack.pop()
+                else:
+                    nnode = String()
                     current.add(nnode)
                     stack.append(nnode)
 
-                case ")":
+            case CharType.Punctuation:
+                match token.content:
+                    case "(":
+                        nnode = Tuple()
+                        if isinstance(current[-1], Expression):
+                            nnode_inner = Call(
+                                [current[-1], nnode],
+                                depth=depth
+                            )
+                            current[-1] = nnode_inner
+                            stack.append(nnode_inner)
+
+                        current.add(nnode)
+                        stack.append(nnode)
+
+                    case ")":
+                        stack.pop()
+
+                    case "[":
+                        nnode = Array()
+                        current.add(nnode)
+                        stack.append(nnode)
+
+                    case "]":
+                        stack.pop()
+
+                    case "#":
+                        nnode = Comment()
+                        current.add(nnode)
+                        stack.append(nnode)
+
+                if token.content in ["+", "-", "*", "**", "/", "//", "%",
+                                     "<", "<=", ">", ">=", "==", "!=",
+                                     "^", "|", "&", "!", "~",
+                                     "->", ".", "..", "@", "+-"]:
+                    # current.add(Operator([token]))
+                    nnode = Operation(
+                        [current[-1], Operator([token], depth=depth)],
+                        # op=Operator([token])
+                        depth=depth
+                    )
+                    current[-1] = nnode
+                    stack.append(nnode)
+
+            case CharType.Letter:
+                current.add(Symbol([token]))
+                if isinstance(current, Operation):
                     stack.pop()
 
-                case "[":
-                    nnode = Array()
-                    current.add(nnode)
-                    stack.append(nnode)
-
-                case "]":
+            case CharType.Digit:
+                current.add(Int([token], depth=depth))
+                if isinstance(current, Operation):
                     stack.pop()
 
-                case "#":
-                    nnode = Comment()
-                    current.add(nnode)
-                    stack.append(nnode)
+            case CharType.Whitespace:
+                pass
 
-            if token.content in ["+", "-", "*", "**", "/", "//", "%",
-                                 "<", "<=", ">", ">=", "==", "!=",
-                                 "^", "|", "&", "!", "~",
-                                 "->", ".", "..", "@", "+-"]:
-                # current.add(Operator([token]))
-                nnode = Operation(
-                    [current[-1], Operator([token], depth=depth)],
-                    # op=Operator([token])
-                    depth=depth
-                )
-                current[-1] = nnode
-                stack.append(nnode)
+            case CharType.Newline:
+                if isinstance(current, Comment):
+                    stack.pop()
 
-        case CharType.Letter:
-            current.add(Symbol([token]))
-            if isinstance(current, Operation):
-                stack.pop()
+            case _:
+                print(tree)
+                print(token)
+                raise SyntaxError
+    return tree
 
-        case CharType.Digit:
-            current.add(Int([token], depth=depth))
-            if isinstance(current, Operation):
-                stack.pop()
 
-        case CharType.Whitespace:
-            pass
-
-        case CharType.Newline:
-            if isinstance(current, Comment):
-                stack.pop()
-
-        case _:
-            print(tree)
-            print(token)
-            raise SyntaxError
+tree = parse(tokens)
 # print(tree)
 tree.print()
