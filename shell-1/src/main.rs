724b2005ddbb5d35f9a287259b3a472ad79ae6cd
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-25 21:18:43 -0400

    implement simple REPL

--- shell-1/src/main.rs
+++ shell-1/src/main.rs
@@ -13,3 +13,24 @@ mod value;
 use lexer::lex;
 use parser::parse;
 use node::Node;
+
+fn main() {
+    let mut input = String::new();
+    // let now = SystemTime::now();
+    let now = Utc::now();
+    println!("shell-1 | {}", now);
+    loop {
+        print!("~ ");
+        io::stdout().flush();
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let tokens = lex(input.clone(), false).unwrap();
+        println!("{:?}", tokens);
+
+        let tree: Node = parse(tokens, false);
+        println!("{:?}", tree);
+
+        println!("{}", tree.evaluate().unwrap());
+    }
+}
