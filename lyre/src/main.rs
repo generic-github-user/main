6030d3345e5976e11af97bf2d5e9b31d11ffe926
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-09-30 23:22:04 -0400

    update lex function to take a String as input

--- lyre/src/main.rs
+++ lyre/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+// use std::fs::File;
+use std::fs;
 use std::io::{BufReader, Error};
 use std::collections::HashMap;
 
@@ -17,13 +18,15 @@ use node::Language;
 
 fn main () -> Result<(), Error> {
     let path = "sample.ly";
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
+    // let input = File::open(path).expect("Could not read source file");
+    // let buffered = BufReader::new(input);
+    // input.read_to_string(&mut source);
 
-    let tokens = lex(buffered, false).unwrap();
+    let source: String = fs::read_to_string(path).expect("Could not read source file");
+    let tokens = lex(source, false).unwrap();
     let root = parse(tokens, false);
 
-    println!("{}", root.transpile(Language::C));
+    println!("{}", root.transpile(Language::C, false));
 
     //Ok(())
     //return Ok(root.evaluate().unwrap());
