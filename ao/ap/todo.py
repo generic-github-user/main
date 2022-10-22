c43451dc2c5a53f175c40d28da5195d27c47b67e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-10-22 02:13:06 -0400

    add docstrings to other List methods

--- ao/ap/todo.py
+++ ao/ap/todo.py
@@ -22,6 +22,8 @@ print(args)
 
 
 class List:
+    """Creates a new list containing the elements from the Python list `items`
+    (i.e., wrapping it)"""
     def __init__(self, items=None):
         if items is None:
             items = []
@@ -36,15 +38,22 @@ class List:
     def filter(self, p):
         return List(list(filter(p, self.items)))
 
+    """Returns a new list containing elements for which the attribute `attr` is
+    equal to `value`"""
     def filter_by(self, attr, value):
         return self.filter(lambda x: getattr(x, attr) == value)
 
+    """Removes items equal to any of the arguments, returning a new list"""
     def remove(self, *args):
         return self.filter(lambda x: x not in args)
 
+    """Returns a new list constructed by accessing the `attr` attribute of each
+    item in this list"""
     def get(self, attr):
         return self.map(lambda x: getattr(x, attr))
 
+    """Returns a new list sorted using the comparison function `f`, which
+    should take as input two elements of the input list"""
     def sorted(self, f):
         return List(list(sorted(self.items, key=f)))
 
@@ -63,9 +72,11 @@ class List:
     def none(self, p):
         return not self.any(p)
 
+    """Returns an integer representing the length of this list"""
     def len(self):
         return len(self.items)
 
+    """Adds a new element to this list (modifying it in place)"""
     def append(self, x):
         return self.items.append(x)
 
@@ -217,6 +228,8 @@ def update_list(todo_list, path):
         # the task to be moved to a different list
         if 'onhold' in item.tags:
             item.location = config.paths.hold
+        if 'ideas' in item.tags:
+            item.location = config.paths.ideas
         if item.done:
             item.location = config.paths.complete
 
