275d2495ad2eddc38e36656dd1c181d406f7ad85
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-10-25 17:34:59 -0400

    fix relative import issue
    (highly unpleasant)

--- ao/ap/todo.py
+++ ao/ap/todo.py
@@ -11,7 +11,11 @@ import dateparser
 import tarfile
 
 from box import Box
-from ...lib.pylist import List
+# from ...lib.pylist import List
+# from main.lib.pylist import List
+# from main.lib import pylist
+from lib.pylist import List
+# print(pylist.List)
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -19,12 +23,13 @@ parser.add_argument('--dry-run', action='store_true',
                     help='Executes a "dry run"; will simulate updating \
                     the todo list but won\'t actually modify any files')
 parser.add_argument('--flush', action='store_true')
+parser.add_argument('--config', type=str)
 args = parser.parse_args()
 print(args)
 
 
 todo_path = os.path.expanduser('~/Desktop/.todo')
-config = Box(yaml.safe_load(Path('config.yaml').read_text()))
+config = Box(yaml.safe_load(Path(args.config).read_text()))
 config.base = Path(config.base_path).expanduser()
 for k, v in config.paths.items():
     # config[k] = os.path.expanduser(v)
