607193a3b657cf7ebb237bb348477187ea508bc0
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-10-25 16:58:31 -0400

    fix spacing in todo.py

--- ao/ap/todo.py
+++ ao/ap/todo.py
@@ -23,8 +23,6 @@ args = parser.parse_args()
 print(args)
 
 
-
-
 todo_path = os.path.expanduser('~/Desktop/.todo')
 config = Box(yaml.safe_load(Path('config.yaml').read_text()))
 config.base = Path(config.base_path).expanduser()
@@ -50,41 +48,51 @@ class todo:
     def __init__(self, raw, content=''):
         # The original text from which this todo item was parsed
         self.raw = raw
+
         # The "text" or content of the todo item (excludes tags and other
         # metadata/markup)
         self.content = content
+
         # A boolean flag indicating whether the corresponding task is complete
         self.done = False
+
         # A timestamp indicating when this item was marked as complete
         # (actually reflects the first occasion on which the script was rerun
         # after the file was modified)
         self.donetime = None
+
         # A (chronologically ordered) set of "snapshots" reflecting every
         # processed todo item that was considered a match to the "canonical"
         # version of the task in the database (currently unused for efficiency
         # reasons)
         self.snapshots = []
+
         # Tags used to mark various properties about the task (item) -- these
         # are sometimes further processed into special attributes like
         # todo.done and todo.duration
         self.tags = []
+
         # unused
         self.source = None
+
         # The time at which the todo instance representing this item was first
         # created
         self.created = time.time()
         self.importance = 0
+
         # This might be used in the future but for now is just somewhat
         # redundant metadata; if we incorporate positional context when
         # analyzing lists we may as well be writing an entire version control
         # system
         self.line = None
+
         # The file (specific todo list) in which the item currently resides;
         # this is often modified during processing so that when the lists are
         # rewritten the item is moved to a new file
         self.location = ''
         self.time = None
         self.duration = None
+
         # Nested tasks/children of this item; currently unused due to parsing
         # limitations (the eventual goal is to integrate the zeal markup
         # parser, though the block indentation parser might be factored out
