8015b9dbaba940d28b0f40ffabcfd31d5709030f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-11-01 15:06:55 -0400

    add basic array methods

--- c-adts/vec.c
+++ c-adts/vec.c
@@ -34,3 +34,33 @@ struct Array {
     Type T;
 };
 typedef struct Array Array;
+
+Option array_free (Array* self) {
+    free(self -> data);
+    self -> size = 0;
+    return None;
+}
+
+Option array_cast (Array* self, Type Q);
+Array new_array (unsigned int size, Type T) {
+    return (Array) {
+        size,
+        size * T.size,
+        &array_free,
+        &array_cast,
+        malloc(size * T.size),
+        T
+    };
+}
+
+Array* new_allocated_array (unsigned int size, Type T) {
+    Array* result = malloc(sizeof(Array));
+    *result = new_array(size, T);
+    return result;
+}
+
+Option array_cast (Array* self, Type Q) {
+    Array* result = new_allocated_array(self -> size, Q);
+    result -> data = self -> data;
+    return Some(result);
+}
