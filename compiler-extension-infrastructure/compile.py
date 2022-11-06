bd140f45705d82e4c05180e48093adcdb1b54713
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-11-06 01:25:00 -0400

    amend previous commit

--- compiler-extension-infrastructure/compile.py
+++ compiler-extension-infrastructure/compile.py
@@ -10,6 +10,9 @@ from typing import Callable, Union
 from token import Token
 from node import Node
 
+from filters import range_filter
+# from resolve_names import resolve_names
+
 # TODO: make diagram of type system(s) and related abstractions
 # language/framework in which assignment is an assertion of equality?
 
@@ -50,91 +53,6 @@ def label_assignments(node):
 
 # def implicit_return(node):
     # if node.type == 'function':
-
-
-def range_filter(node):
-    """This filter handles desugaring of the ".." syntax, which is modelled
-    loosely after Rust's. The operator is transformed into a call to the
-    `range` function, which returns a particular kind of iterator. If the
-    traits system and/or iterator trait/type are not used in the compilation
-    process, it can be further rewritten into a standalone loop.
-
-    This filter is also used as an example of a generic filter in `README.md`,
-    and is helpful for demonstrating the purpose of the compiler extension
-    infrastructure project."""
-
-    # if isinstance(node, Node):
-    #     print(node.type, node.op == '..')
-    if node.type == 'bin_op' and node.op == '..':
-        # we could also use string interpolation in combination with a parser
-        # call, but this way is conceptually cleaner
-        return Node(type_='call', children=[
-            Token(type_='IDENTIFIER', value='range'),
-            Node(type_='tuple', depth=node.depth+1, children=[
-                node.left, node.right], parent=node.parent)
-        ], depth=node.depth, parent=node.parent)
-    return node
-
-
-def resolve_names(node, namespace):
-    """Replaces identifiers in particular kinds of locations ("evaluation
-    contexts") with references to their values. This should generally happen
-    before type inference is attempted."""
-
-    if node.type == 'function_declaration':
-        assert hasattr(node, 'signature') and node.signature is not None
-        assert hasattr(node.signature, 'arguments') and\
-            node.signature.arguments is not None
-        fnode = Node(type_='function', vtype='function',
-                     return_type=node.return_type,
-                     definition=node,
-                     name=node.signature.name,
-                     signature=node.signature,
-                     body=node.body,
-                     arguments=node.signature.arguments,
-                     arity=node.signature.arguments.children.len(),
-                     parent=node.parent)
-        namespace[node.name] = fnode
-        for p in fnode.arguments.children:
-            namespace[p.name] = None
-        # breakpoint()
-        result = node.with_attr('body', node.body.resolve_names(namespace))
-        fnode.arguments.children.map(lambda x: namespace.pop(x.name))
-        return result
-
-    if node.type in ['start', 'program', 'form', 'block', 'statement',
-                     'call', 'expression', 'declaration', 'expression']:
-        print(f'Resolving names in {node.type}')
-        return node.with_attr('children',
-                              node.children.map(lambda x: x.resolve_names(namespace)))
-
-    if node.type in ['operation', 'tuple', 'bin_op', 'literal']:
-        print(f'Resolving names in {node.type}')
-        return node.with_attr('children',
-                              node.children.map(lambda x: x.resolve_names(namespace)))
-
-    if node.type in ['OPERATOR', 'STRING', 'INT']:
-        return node
-
-    if node.type == 'IDENTIFIER':
-        assert isinstance(node, Token)
-        print(f'Dereferencing name {node.value}')
-        if node.value not in namespace:
-            raise_error('name resolution', f"""\
-                        {node.value} not defined when used at line {node.line};
-                        make sure that name is defined and in scope.""")
-        # breakpoint()
-        return namespace[node.value]
-
-    raise NotImplementedError(node.type)
-    # self.children.map(lambda x : x.infer_types())
-    # return node
-
-
-def raise_error(etype, message):
-    wrapped = textwrap.dedent(message).replace("\n", " ")
-    print(f'Compiler error ({etype} error): {wrapped}')
-    quit()
 
 
 class IRNode:
