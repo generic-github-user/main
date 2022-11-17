da3fa0c4e039c2f143ba8dc79c1845591ac959d2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-11-17 03:23:48 -0500

    add helper class for iterating over String instances

--- lib/python/pystring.py
+++ lib/python/pystring.py
@@ -1,3 +1,27 @@
+from lib.pyiter import Iter
+
+
+class StringIter(Iter):
+    def __init__(self, string):
+        self.i = 0
+        self.string = string
+
+    def current(self):
+        return self.string[self.i]
+
+    def __next__(self):
+        if self.n < (self.string.len() - 1):
+            self.n += 1
+            return self.n
+        return None
+
+    def to_iter(self):
+        return Iter(self, self.n, lambda S: self.__next__())
+
+    def __str__(self):
+        return f'StringIter <i = {self.i}>'
+
+
 class String:
     def __init__(self, s=''):
         if isinstance(s, String):
@@ -6,6 +30,12 @@ class String:
 
     def to_string(self):
         return self
+
+    def iter(self):
+        return StringIter(self)
+
+    def len(self):
+        return len(self.s)
 
     def __iadd__(self, other):
         if isinstance(other, String):
