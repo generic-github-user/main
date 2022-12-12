89137788da2f82cba3ef3fd9b2d5bd71462fdb2b
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-12-12 01:04:50 -0500

    add wrapper/decorator for concisely declaring tasks

--- cw-archiver/cw.py
+++ cw-archiver/cw.py
@@ -9,6 +9,22 @@ import itertools
 from config import cookies, headers, groups
 groups = BoxList(groups)
 
+# tasks = []
+
+def task(db, group):
+    def task_factory(f):
+        def task_wrapper(*args, **kwargs):
+            result = f(*args, **kwargs)
+            if time.time() - db.checkpoint > 30:
+                print("Saving checkpoint")
+                db.to_json(filename=group.path)
+                db.checkpoint = time.time()
+            time.sleep(0.5)
+            return result
+        return task_wrapper
+    return task_factory
+
+# @task
 def fetch_post(db, group, post, attr):
     try:
         print(f"Fetching /{attr} attribute for post: {post.title}")
@@ -25,15 +41,31 @@ def fetch_post(db, group, post, attr):
             post[attr] = data
         else:
             print(response)
-        time.sleep(0.5)
     except requests.exceptions.ConnectionError as ex:
         print(ex)
 
-    if time.time() - db.checkpoint > 30:
-        print("Saving checkpoint")
-        db.to_json(filename=group.path)
-        db.checkpoint = time.time()
+def fetch_batch(db, group):
+    posts = db.posts
+    n = 20
+    if posts: t = posts[-1].createdAt
+    elif "last_batch" in db: t = db.last_batch
+    else: t = "2022-12-30T00:00:00.000000Z"
+    print(f"Fetching batch: {t}")
 
+    response = requests.get(
+        f'https://api.campuswire.com/v1/group/{group.id}/posts?number={n}&before={t}',
+        cookies=cookies,
+        headers=headers,
+    )
+    data = BoxList(response.json())
+    db.posts.extend(data)
+    db.last_batch = t
+    print(f"Received {len(data)} posts")
+    if len(data) == 0:
+        return -1
+
+
+# @task
 def update_class(group):
     print(f"Fetching {group}")
     # db = BoxList(json.loads(Path(path).read_text()))
@@ -49,32 +81,16 @@ def update_class(group):
         print(ex)
         db = Box(posts=[])
 
-    posts = db.posts
-    n = 20
     for i in range(50):
-        if posts: t = posts[-1].createdAt
-        elif "last_batch" in db: t = db.last_batch
-        else: t = "2022-12-30T00:00:00.000000Z"
-        print(f"Fetching batch: {t}")
-
-        response = requests.get(
-            f'https://api.campuswire.com/v1/group/{group.id}/posts?number={n}&before={t}',
-            cookies=cookies,
-            headers=headers,
-        )
-        data = BoxList(response.json())
-        db.posts.extend(data)
-        db.last_batch = t
-        if len(data) == 0:
+        if task(db, group)(fetch_batch)(db, group) == -1:
             break
 
         # pprint.pprint(data[0].to_dict())
-        time.sleep(0.5)
 
     db.checkpoint = time.time()
     for attr in group.attrs:
         for post in itertools.islice(filter(lambda x: attr not in x, db.posts), 200):
-            fetch_post(db, group, post, attr)
+            task(db, group)(fetch_post)(db, group, post, attr)
 
     db.to_json(filename=group.path)
 
