4aa463c9892b20800ac9b532fbd04da97ca3e41b
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-12-11 19:28:14 -0500

    update script to allow fetching multiple courses/groups

--- cw-archiver/cw.py
+++ cw-archiver/cw.py
@@ -6,57 +6,77 @@ from pathlib import Path
 import json
 import itertools
 
-from config import cookies, headers, path, group
+from config import cookies, headers, groups
+groups = BoxList(groups)
 
-# db = BoxList(json.loads(Path(path).read_text()))
-with open(path, "r") as db_file:
+def update_class(group):
+    print(f"Fetching {group}")
+    # db = BoxList(json.loads(Path(path).read_text()))
     try:
-        db = Box(json.load(db_file))
-    except json.decoder.JSONDecodeError as ex:
+        with open(group.path, "r") as db_file:
+            try:
+                db = Box(json.load(db_file))
+            except json.decoder.JSONDecodeError as ex:
+                print(ex)
+                quit()
+                db = Box(posts=[])
+    except FileNotFoundError as ex:
         print(ex)
-        quit()
         db = Box(posts=[])
 
-posts = db.posts
-n = 20
-for i in range(50):
-    if posts: t = posts[-1].createdAt
-    elif "last_batch" in db: t = db.last_batch
-    else: t = "2022-12-30T00:00:00.000000Z"
-    print(f"Fetching batch: {t}")
+    posts = db.posts
+    n = 20
+    for i in range(50):
+        # breakpoint()
+        if posts: t = posts[-1].createdAt
+        elif "last_batch" in db: t = db.last_batch
+        else: t = "2022-12-30T00:00:00.000000Z"
+        print(f"Fetching batch: {t}")
 
-    response = requests.get(
-        f'https://api.campuswire.com/v1/group/{group}/posts?number={n}&before={t}',
-        cookies=cookies,
-        headers=headers,
-    )
-    # breakpoint()
-    data = BoxList(response.json())
-    db.posts.extend(data)
-    db.last_batch = t
-    if len(data) == 0:
-        break
+        response = requests.get(
+            f'https://api.campuswire.com/v1/group/{group.id}/posts?number={n}&before={t}',
+            cookies=cookies,
+            headers=headers,
+        )
+        # print(response.text)
+        # breakpoint()
+        data = BoxList(response.json())
+        db.posts.extend(data)
+        db.last_batch = t
+        if len(data) == 0:
+            break
 
-    # pprint.pprint(data[0].to_dict())
-    time.sleep(0.5)
+        # pprint.pprint(data[0].to_dict())
+        time.sleep(0.5)
 
-checkpoint = time.time()
-for attr in ["uniqueviews", "comments"]:
-    for post in itertools.islice(filter(lambda x: attr not in x, db.posts), 200):
-        try:
-            print(f"Fetching /{attr} attribute for post: {post.title}")
-            endpoint = f'https://api.campuswire.com/v1/group/{group}/posts/{post.id}/{attr}'
-            response = requests.get(endpoint, cookies=cookies, headers=headers)
-            data = BoxList(response.json())
+    checkpoint = time.time()
+    for attr in group.attrs:
+        for post in itertools.islice(filter(lambda x: attr not in x, db.posts), 200):
+            try:
+                print(f"Fetching /{attr} attribute for post: {post.title}")
+                endpoint = f'https://api.campuswire.com/v1/group/{group.id}/posts/{post.id}/{attr}'
+                response = requests.get(endpoint, cookies=cookies, headers=headers)
+                if response.status_code != 404:
+                    try:
+                        data = BoxList(response.json())
+                    except requests.exceptions.JSONDecodeError as ex:
+                        breakpoint()
+                        print(ex)
+                        # print(response)
 
-            post[attr] = data
-            time.sleep(0.5)
-        except requests.exceptions.ConnectionError as ex:
-            print(ex)
+                    post[attr] = data
+                else:
+                    print(response)
+                time.sleep(0.5)
+            except requests.exceptions.ConnectionError as ex:
+                print(ex)
 
-        if time.time() - checkpoint > 30:
-            print("Saving checkpoint")
-            db.to_json(filename=path)
-            checkpoint = time.time()
+            if time.time() - checkpoint > 30:
+                print("Saving checkpoint")
+                db.to_json(filename=group.path)
+                checkpoint = time.time()
 
-db.to_json(filename=path)
+    db.to_json(filename=group.path)
+
+for G in groups:
+    update_class(G)
