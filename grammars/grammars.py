62f31c2b4812a4d9f622754a5edd6319e1242661
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2023-02-03 16:18:31 -0500

    add basic parsing functionality

--- grammars/grammars.py
+++ grammars/grammars.py
@@ -59,6 +59,9 @@ class Range(Generic[T]):
     def to_iter(self):
         return Iter(self, self.n, lambda S: self.__next__())
 
+    def __contains__(self, x) -> bool:
+        return self.a <= x <= self.b
+
     def __str__(self):
         return f'Range [ {self.a}..{self.b} ]'
 
@@ -66,6 +69,7 @@ class Range(Generic[T]):
         return f'{self.a}..{self.b}'
 
     def __add__(x, y):
+        if isinstance(y, int): return Range(x.a + y, x.b + y)
         return Range(x.a + y.a, x.b + y.b)
 
     def __radd__(x, y):
@@ -73,8 +77,18 @@ class Range(Generic[T]):
             return Range(x.a + y, x.b + y)
         raise NotImplementedError
 
+    def __sub__(x, y):
+        if isinstance(y, int): return Range(x.a - y, x.b - y)
+        return Range(x.a - y.a, x.b - y.b)
+
     def __mul__(x, y):
         return Range(x.a * y.a, x.b * y.b)
+
+    def __lt__(self, x):
+        return self.b < x
+
+    def __gt__(self, x):
+        return self.a > x
 
 class OrderedSet:
     def __init__(self, data):
@@ -133,6 +147,12 @@ class Choice(Rule):
         if len(self.options) == 0: return 0
         return Mean([x.expected_length() for x in self.options])
 
+    def match(self, x: str) -> bool:
+        return any(rule.match(x) for rule in self.options)
+
+    def partial_match(self, x: str) -> set[int]:
+        return set.union(*[rule.partial_match(x) for rule in self.options])
+
     def __add__(self, b: Choice) -> Choice:
         return Choice(list(OrderedSet(self.options) | OrderedSet(b.options)))
 
@@ -161,6 +181,15 @@ class Terminal(Rule):
 
     def expected_length(self) -> float:
         return len(self.value)
+
+    def match(self, x: str) -> bool:
+        return self.value == x
+
+    def partial_match(self, x: str) -> set[int]:
+        if x.startswith(self.value):
+            # return set([len(x)])
+            return set([len(self.value)])
+        return set()
 
     def __repr__(self) -> str:
         return f'"{self.value}"'
@@ -204,6 +233,32 @@ class Repeat(Rule):
             return self.rule.expected_length() * self.n / 2 # ??
         return self.rule.expected_length() * self.n.midpoint()
 
+    def match(self, x: str) -> bool:
+        # TODO: unfuck
+        # ?
+        # if not self.n > 0 and x != '': return False
+        # should the check be inverted (string-based)?
+        # TODO: BoundedRange type
+        if (self.n < 1 if isinstance(self.n, int) else self.n.b < 1):
+            return x == ''
+            # return self.rule.match(x)
+        if x == '': return (self.n == 0 if isinstance(self.n, int) else 0 in self.n)
+        if (match := self.rule.partial_match(x)):
+            # print(f'Matched "{x}" under {self} at {match}')
+            return any(Repeat(self.rule, self.n - 1).match(x[i:]) for i in match)
+        # print(f'Rejected "{x}" under {self}')
+        return False
+
+    def partial_match(self, x: str) -> set[int]:
+        # raise NotImplementedError
+        if (self.n < 1 if isinstance(self.n, int) else self.n.b < 1):
+            # return set([0]) if x == '' else set()
+            return set([0])
+        if (matches := self.rule.partial_match(x)):
+            status = (self.n == 1 if isinstance(self.n, int) else self.n.a == 1)
+            return set.union(*[set(j + i for j in Repeat(self.rule, self.n - 1).partial_match(x[i:])) for i in matches] + ([matches] if status else []))
+        return set()
+
     def __repr__(self) -> str:
         return f'{self.rule} {{ {self.n} }}'
 
@@ -219,6 +274,13 @@ def Optional(source: Rule) -> Repeat:
 
 def Star(source: Rule) -> Repeat:
     return Repeat(source, Range(0, math.inf))
+
+class Empty(Rule):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def match(self, x: str) -> bool:
+        return x == ''
 
 class Sequence(Rule):
     def __init__(self, rules: list[Rule]) -> None:
@@ -242,6 +304,23 @@ class Sequence(Rule):
     def expected_length(self) -> float:
         if len(self.rules) == 0: return 0
         return Mean([unravel(x).expected_length() for x in self.rules])
+
+    def match(self, x: str) -> bool:
+        # if len(self.rules) == 0: return False
+        return bool((m := self.partial_match(x)) and any(i >= len(x) for i in m))
+
+    def partial_match(self, x: str) -> set[int]:
+        if len(self.rules) == 0:
+            # return set([0]) if x == '' else set() # ?
+            return set([0])
+        if (matches := self.rules[0].partial_match(x)):
+            # print(f'Matched "{x}" under {self} at {matches}')
+            # TODO: add offset to indices (?)
+            if len(self.rules) == 1: return matches
+            # ?
+            return set.union(*[set(j + i for j in self[1:].partial_match(x[i:])) for i in matches])
+        # print(f'Rejected "{x}" under {self}')
+        return set()
 
     def __getitem__(self, i) -> Sequence:
         return Sequence(self.rules[i])
@@ -318,3 +397,28 @@ print(recursive.sample())
 # print(recursive.expected_length())
 # for x in itertools.islice(recursive.iter(), 10):
     # print(x)
+
+# print(test.match('a'))
+# print(test.match('apple'))
+# print(Terminal('a').match('a'))
+
+def test_match(rule, s):
+    print(f'{rule} / {s} / {rule.match(s)} / {rule.partial_match(s)}')
+
+test_match(Terminal('a'), 'a')
+test_match(test, 'a')
+test_match(test, 'apple')
+test_match((Terminal('a') * 5), 'aaaaa')
+test_match((Terminal('a') * 5), 'aaaa')
+test_match((Terminal('a') * 5), 'aaaaaa')
+test_match((Terminal('a') * Range(3, 10)), 'aaaaaa')
+test_match((Terminal('a') * Range(3, 10)), 'aa')
+test_match(Terminals('xyz'), 'a')
+test_match(Terminals('xyz'), 'y')
+test_match(Terminals('ab') & Terminals('cd'), 'ad')
+test_match(Terminals('ab') & Terminals('cd'), 'be')
+
+test_match(vowels, 'a')
+test_match(syllable, 'a')
+test_match(Sequence([Optional(vowels), vowels]), 'a')
+test_match(Optional(vowels), 'a')
