72ab8ece5fae982345e48458c6f4d180fe1f210c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2023-02-02 20:35:12 -0500

    hack in some methods for combination of Range objects
    ... and arithmetic operations involving other types (still needs a lot
    of work)

--- grammars/grammars.py
+++ grammars/grammars.py
@@ -3,15 +3,25 @@ from __future__ import annotations
 import string
 import random
 import itertools
+import math
+import functools
 
 from typing import TypeVar, Generic, Any, Iterable, Iterator
 T = TypeVar('T')
+
+def unravel(z):
+    if callable(z): return z()
+    return z
 
 class Range(Generic[T]):
     def __init__(self: Range[T], a: T, b: T) -> None:
         assert b >= a
         self.a = a
         self.b = b
+
+    @staticmethod
+    def from_scalar(x: T) -> Range[T]:
+        return Range(x, x)
 
     def contains(self, x: T) -> bool:
         return x >= self.a and x <= self.b
@@ -22,6 +32,17 @@ class Range(Generic[T]):
 
     def iter(self):
         return range(self.a, self.b+1)
+
+    def join_(self: Range[T], x: T | Range[T]) -> Range[T]:
+        if isinstance(x, int):
+            return Range(min(self.a, x), max(self.b, x))
+        # ?
+        return Range(min(self.a, x.a), max(self.b, x.b))
+
+    @staticmethod
+    def join(ranges: Iterable[T | Range[T]]) -> Range[T]:
+        # return Range(min(x.a for x in ranges), max(x.b for x in ranges))
+        return functools.reduce(Range.join_, ranges)
 
     def __next__(self):
         if self.n < (self.bounds[1] - 1):
@@ -34,3 +55,17 @@ class Range(Generic[T]):
 
     def __str__(self):
         return f'Range [ {self.a}..{self.b} ]'
+
+    def __repr__(self):
+        return f'{self.a}..{self.b}'
+
+    def __add__(x, y):
+        return Range(x.a + y.a, x.b + y.b)
+
+    def __radd__(x, y):
+        if isinstance(y, int):
+            return Range(x.a + y, x.b + y)
+        raise NotImplementedError
+
+    def __mul__(x, y):
+        return Range(x.a * y.a, x.b * y.b)
